36fdff88a7a4214dd46504d79291eac5839ff76e
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-05 15:00:13 -0600

    created drop tables for the beginning of sql https://app.asana.com/0/161555263951762/164464506248117

---
+++ sql/flek.sql
@@ -0,0 +1,13 @@
+-- SQL statements for drop tables
+DROP TABLE IF EXISTS favorite;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS hashtag;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS social login;
+DROP TABLE IF EXISTS mail;
+DROP TABLE IF EXISTS profile;
+-- End of SQL statements
+-- not sure why profile is still orange
+
+
