64c5a45dd2e8a734654368916c09c389451aaf35
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-06 11:29:40 -0600

    organized order of drop tables and added space to comments line 22 -- foo https://app.asana.com/0/161555263951762/164464506248117

--- sql/flek.sql
+++ sql/flek.sql
@@ -10,46 +10,48 @@ DROP TABLE IF EXISTS profile;
 -- End of SQL statements
 
 
--- FAVORITE drop table here
-
-
--- TAG drop table here
-
-
--- HASHTAG drop table here
-
-
--- GENRE drop table here
-CREATE TABLE genre (
-	genreId INT UNSIGNED NOT NULL,
-	genreName VARCHAR(32) NOT NULL,
-	UNIQUE (genreId),
-	PRIMARY KEY (genreId)
+-- PROFILE drop table here
+CREATE TABLE profile (
+  profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  profileName VARCHAR(32) NOT NULL,
+  profileEmail VARCHAR(128) NOT NULL,
+  profileLocation VARCHAR (64) NOT NULL,
+  profileBio VARCHAR (250) NOT NULL,
+  profileHash CHAR(128) NOT NULL,
+  profileSalt CHAR(64) NOT NULL,
+  profileAccessToken INT UNSIGNED NULL, -- is this correct?--
+  profileActivationToken CHAR(32), -- is this a hash?--
+UNIQUE (userEmail),
+  PRIMARY KEY (profileId)
 );
 
--- IMAGE drop table here
+-- MAIL drop table here
 
 
 -- SOCIAL LOGIN drop table here
 
 
--- MAIL drop table here
+-- IMAGE drop table here
 
 
--- PROFILE drop table here
-CREATE TABLE profile (
-	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	profileName VARCHAR(32) NOT NULL,
-	profileEmail VARCHAR(128) NOT NULL,
-	profileLocation VARCHAR (64) NOT NULL,
-	profileBio VARCHAR (250) NOT NULL,
-	profileHash CHAR(128) NOT NULL,
-	profileSalt CHAR(64) NOT NULL,
-	profileAccessToken INT UNSIGNED NULL, --is this correct?--
-	profileActivationToken CHAR(32), --is this a hash?--
-	UNIQUE (userEmail),
-	PRIMARY KEY (profileId)
+-- GENRE drop table here
+CREATE TABLE genre (
+  genreId INT UNSIGNED NOT NULL,
+  genreName VARCHAR(32) NOT NULL,
+  UNIQUE (genreId),
+  PRIMARY KEY (genreId)
 );
+
+
+-- HASHTAG drop table here
+
+
+
+-- TAG drop table here
+
+
+
+-- FAVORITE drop table here
 
 
 
