f302c1ccf15dda663cbd7140e0a14fb2e1e180b5
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-06 10:59:37 -0600

    switched order of drop table if exists - switched genre & image place https://app.asana.com/0/161555263951762/164464506248117

--- sql/flek.sql
+++ sql/flek.sql
@@ -2,12 +2,14 @@
 DROP TABLE IF EXISTS favorite;
 DROP TABLE IF EXISTS tag;
 DROP TABLE IF EXISTS hashtag;
-DROP TABLE IF EXISTS image;
 DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS image;
 DROP TABLE IF EXISTS socialLogin;
 DROP TABLE IF EXISTS mail;
 DROP TABLE IF EXISTS profile;
 -- End of SQL statements
 -- not sure why profile is still orange
+
+
 
 
