8cac01c5ef60e91d01e1d188c70484b51c44e273
Christina Sosa <csosa2106@gmail.com>
2016-08-06 22:44:22 -0600

    created public function construct in profile class https://app.asana.com/0/161555263951762/164443198012112

--- public_html/php/classes/Profile.php
+++ public_html/php/classes/Profile.php
@@ -63,14 +63,47 @@ private profileActivationToken
 
 	/**
 	 * constructor for profile
-	 * @param int
-	 * @param
-	 * @param
-	 * @param
-	 * @param
-	 * @param
-	 * @param
-	 * @param
-	 * @param
+	 * @param int|null $newProfileId of the profiler user or null if new profile
+	 * @param string $newProfileName string containing actual profile user full name
+	 * @param string $newProfileEmail string containing profile email
+	 * @param string $newProfileLocation string containing profile physical location
+	 * @param string $newProfileBio string containing profile bio summary
+	 * @param string $newProfileHash string containing actual profile password hash
+	 * @param string $newProfileSalt string containing actual profile password salt
+	 * @param int $newProfileAccessToken
+	 * @param string $newProfileActivationToken string with profile token
+	 * @throws \Exception if some other exception occurs
+	 * @throws \TypeError if data types violate type hints
 	 **/
-}
+	public function __construct (int $newProfileId = null, int $newProfileAccessToken, string $newProfileActivation)
+		try {
+				$this->setProfileId($newProfileId);
+				$this->setProfileName($newProfileId);
+				$this->setProfileLocation($newProfileLocation);
+				$this->setProfileBio($newProfileBio);
+				$this->setProfileHash($newProfileHash);
+				$this->setProfileSalt($newProfileSalt);
+				$this->setProfileAccessToken($newProfileAccessToken);
+				$this->setProfileActivationToken($newProfileActivationToken);
+	} catch(\InvalidArgumentException $invalidArgument) {
+			//rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+	} catch(\RangeException $range) {
+			//rethrow exception to caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+	} catch(\TypeError $typeError) {
+			//rethrow exception to caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+	} catch(\Exception $exception) {
+			//rethrow regular exception to caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+	}
+	//}
+
+/**
+ * accessor method for profile id
+ *
+ * @return int|null value of profile id
+ **/
+
+} //does this curly go on line 101?
