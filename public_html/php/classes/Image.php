e4feb9cdbef21d4ca1c419a430684058364a7155
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-07 15:02:25 -0600

    added namespace and few comments https://app.asana.com/0/161555263951762/164443198012126

--- public_html/php/classes/Image.php
+++ public_html/php/classes/Image.php
@@ -1,7 +1,10 @@
 <?php
+namespace Edu\Cnm\flek;
+
+require_once("autoload.php");
 /**
- * Created by PhpStorm.
- * User: BackInUse
- * Date: 8/7/2016
- * Time: 2:54 PM
+ *
+ * This image can be a small example when the images are stored in the Profile of the user.
+ * @author Diane Peshlakai <dpeshlakai3@cnm.edu>
+ * @version 3.0.
  */
