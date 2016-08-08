53da3451b2a1b24f95bcc43ba88ca9c88430ccae
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-08 08:43:54 -0600

    added classes for favorite https://app.asana.com/0/161555263951762/164443198012126

--- public_html/php/classes/Favorite.php
+++ public_html/php/classes/Favorite.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: BackInUse
- * Date: 8/7/2016
- * Time: 2:54 PM
+namespace Edu\Cnm\Flek;
+
+require_once("autoload.php");
+/*
+ * This favorite can be considered a small example of what could be stored when profiles
+ *
+ * @author Diane Peshlakai <dpeshlakai3@cnm.edu>
+ * @version 3.0.0
  */
+class Favorite implements \JsonSerializable {
+	/*
+	 * id for Favorite when profile can favorite the profile and identify the profile
+	 *  this is the foreign key
+	 * @var int $favoriteeId
+	 */
+	private $favoriteeId;
+	/*
+	 * int $favoriterId
+	 * id for the favorite the profile can favorite vice versa
+	 * this is a foreign key
+	 */
+	private $favoriterId;
+}
