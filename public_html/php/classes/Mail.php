be3481296ca83a1e48ec7945b807d891d123b4eb
Robert Harding <robisonfire@gmail.com>
2016-08-08 13:52:17 -0600

    made private fields for Mail.php https://app.asana.com/0/161552024814145/164443198012114

--- public_html/php/classes/Mail.php
+++ public_html/php/classes/Mail.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: robis_000
- * Date: 8/8/2016
- * Time: 1:27 PM
- */
+
+
+namespace Edu\Cnm\Flek;
+
+
+require_once("autoload.php");
+
+class Mail implements \JsonSerializable {
+	/*this is the primary id for the class*/
+	private mailId;
+	/*this is the subject for a specific message*/
+	private mailSubject;
+	/*this is the profileId of the user who sends the message*/
+	private mailSenderId;
+	/*this is the profileId of the user who reads the message*/
+	private mailReceiverId;
+	/*this is the Id created by mailgun*/
+	private mailGunId;
+	/*this is the actual content of the message*/
+	private mailContent;
+
+
+}
