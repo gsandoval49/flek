48da0bae9381f5c091be0fe6dcd34482130f3aaf
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-08 17:09:40 -0600

    added comment block for JSON serialize, but unsure if i need it since we have no date... https://app.asana.com/0/161555263951762/164443198012122

--- public_html/php/classes/Hashtag.php
+++ public_html/php/classes/Hashtag.php
@@ -299,30 +299,9 @@ class Hashtag implements \JsonSerializable {
         return ($hashtags);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {
     /**
-     * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     * @since 5.4.0
-     */
-    function jsonSerialize()
-    {
-
-    }
-}
+     * Specify data which should be serialized to JSON serialization
+     *
+     * @return array resulting state variable to serialize
+     **/
 }
