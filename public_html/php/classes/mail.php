b4751da76b023eff1e71cb8c19624b88d46489d4
Robert Harding <robisonfire@gmail.com>
2016-08-08 10:32:47 -0600

    added public and private attributes to mail.php https://app.asana.com/0/161552024814145/164443198012114

--- public_html/php/classes/mail.php
+++ public_html/php/classes/mail.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace Edu\Cnm\Flek;
+
+
+require_once("autoload.php");
+
+class mail implements \JsonSerializable {
+	private mailId;
+	private mailSenderId;
+	private mailReceiverId;
+	private mailGunId;
+	public mailContent;
+	public mailSubject;
+
+}
