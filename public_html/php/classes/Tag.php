22dd55d635f36ce37c67a244df9e2ed88612350e
Robert Harding <robisonfire@gmail.com>
2016-08-09 09:52:10 -0600

    finished accessor and mutators for Tag class https://app.asana.com/0/161552024814145/164443198012124

--- public_html/php/classes/Tag.php
+++ public_html/php/classes/Tag.php
@@ -23,13 +23,24 @@ class Tag implements \JsonSerializable {
 	}
 
 	public function setTagImageId(int $newTagImageId){
-		if($newTagImageIdId <= 0) {
+		if($newTagImageId <= 0) {
 			throw(new \RangeException ("image id is not positive"));
 		}
 		/*convert and store the image id*/
 		$this->tagImageId = $newTagImageId;
 	}
 
+	public function getTagHashtagId(){
+		return($this->tagHashtagId);
 	}
 
-}
+	public function setTagHashtagId(int $newTagHashtagId){
+		if($newTagHashtagId <= 0) {
+			throw(new \RangeException ("hashtag id is not positive"));
+		}
+		/*convert and store the hashtag id*/
+		$this->tagHashtagId = $newTagHashtagId;
+	}
+
+	}
+
