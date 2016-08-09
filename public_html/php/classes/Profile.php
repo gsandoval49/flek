985db06dafac9068a1af190dc67eb163ea38fd0b
Christina Sosa <csosa2106@gmail.com>
2016-08-09 10:46:43 -0600

    fixed errors in all accessors and mutators (with Giles) https://app.asana.com/0/161555263951762/164443198012112

--- public_html/php/classes/Profile.php
+++ public_html/php/classes/Profile.php
@@ -6,10 +6,12 @@ namespace Edu\Cnm\Flek;
 require_once("autoload.php");
 
 /**
- * This profile class will offered to users who would like to create a landing page with their information and
+ * This profile class will be offered to users who would like to create a landing page with their information and
  * communicate with others.
  *
  * @author Chrisitna Sosa <csosa4@cnm.edu>
+ *
+ * @version 1.0.0
 **/
 
 class Profile implements \JsonSerializable {
@@ -18,97 +20,98 @@ class Profile implements \JsonSerializable {
 	 * id for the profile is the primary key
 	 * @var int $profileId
 	 **/
-private profileId;
+	private profileId;
 
 	/**
 	 * name of profile
 	 * @var string $profileName
 	 **/
-private profileName;
+	private profileName;
 
 	/**
 	 * email of profile
 	 * @var string $profileEmail
 	 **/
-private profileEmail;
+	private profileEmail;
 
 	/**
 	 * name of city where profile is located
 	 * @var string $profileLocation
 	 **/
-private profileLocation;
+	private profileLocation;
 
 	/**
 	 * content in biography that profile will have
 	 * @var string $profileBio
 	 **/
-private profileBio;
+	private profileBio;
 
 	/**
 	 * name of profileHash
 	 * @var string $profileHash
 	 **/
-private profileHash;
+	private profileHash;
 
 	/**
 	 * name of profileSalt
 	 * @var string $profileSalt
 	 **/
-private profileSalt;
+	private profileSalt;
 
 	/**
 	 * profile access token
-	 * @var int $profileAccessToken
+	 * @var string $profileAccessToken
 	 **/
-private profileAccessToken
+	private profileAccessToken;
 
 	/**
 	 *profile activation token hex
 	 * @var string $profileActivationToken
 	 **/
-private profileActivationToken
+	private profileActivationToken;
 
 	/**
 	 * constructor for profile
-	 * @param int|null $newProfileId of the profil or null if new profile
+	 * @param int|null $newProfileId of the profile or null if new profile
 	 * @param string $newProfileName string containing actual profile full name
 	 * @param string $newProfileEmail string containing profile email
 	 * @param string $newProfileLocation string containing profile physical location
 	 * @param string $newProfileBio string containing profile bio summary
 	 * @param string $newProfileHash string containing actual profile password hash
 	 * @param string $newProfileSalt string containing actual profile password salt
-	 * @param int $newProfileAccessToken
+	 * @param string $newProfileAccessToken string with profile permission
 	 * @param string $newProfileActivationToken string with profile token
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
 	 * @throws \Exception if some other exception occurs
 	 * @throws \TypeError if data types violate type hints
 	 **/
-	public function __construct(int $newProfileId = null, int $newProfileAccessToken, string $newProfileActivation)
-		try {
-				$this->setProfileId($newProfileId);
-				$this->setProfileName($newProfileId);
-				$this->setProfileLocation($newProfileLocation);
-				$this->setProfileBio($newProfileBio);
-				$this->setProfileHash($newProfileHash);
-				$this->setProfileSalt($newProfileSalt);
-				$this->setProfileAccessToken($newProfileAccessToken);
-				$this->setProfileActivationToken($newProfileActivationToken);
+	public function __construct(int $newProfileId = null, string $newProfileName, string $newProfileLocation, string
+	$newProfileBio, string $newProfileHash, string $newProfileSalt, string $newProfileAccessToken, string
+	$newProfileActivationToken) {
+			try {
+					$this->setProfileId($newProfileId);
+					$this->setProfileName($newProfileName);
+					$this->setProfileLocation($newProfileLocation);
+					$this->setProfileBio($newProfileBio);
+					$this->setProfileHash($newProfileHash);
+					$this->setProfileSalt($newProfileSalt);
+					$this->setProfileAccessToken($newProfileAccessToken);
+					$this->setProfileActivationToken($newProfileActivationToken);
+			} catch (\InvalidArgumentException $invalidArgument) {
+					//rethrow the exception to the caller
+					throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+			} catch(\RangeException $range) {
+					//rethrow exception to caller
+					throw(new \RangeException($range->getMessage(), 0, $range));
+			} catch(\TypeError $typeError) {
+					//rethrow exception to caller
+					throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+			} catch(\Exception $exception) {
+					//rethrow regular exception to caller
+					throw(new \Exception($exception->getMessage(), 0, $exception));
+			}
 	}
-
-catch
-(\InvalidArgumentException $invalidArgument) {
-	//rethrow the exception to the caller
-	throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
-} catch(\RangeException $range) {
-	//rethrow exception to caller
-	throw(new \RangeException($range->getMessage(), 0, $range));
-} catch(\TypeError $typeError) {
-	//rethrow exception to caller
-	throw(new \TypeError($typeError->getMessage(), 0, $typeError));
-} catch(\Exception $exception) {
-	//rethrow regular exception to caller
-	throw(new \Exception($exception->getMessage(), 0, $exception));
-}
-	//}
 
 /**
  * accessor method for profile id
@@ -116,7 +119,7 @@ catch
  * @return int|null value of profile id
  **/
 public function getProfileId() {
-	return ($this->profileId);
+		return ($this->profileId);
 }
 
 /**
@@ -127,19 +130,19 @@ public function getProfileId() {
  * @throws \TypeError if $newProfileId is not an integer
  **/
 public function setProfileId(int $newProfileId = null) {
-	//base case: if the profile id is null, this is a new profile without a mySQL assigned it (yet)
-	if($newProfileId === null) {
-		$this->profileId = null;
-		return;
-	}
+		//base case: if the profile id is null, this is a new profile without a mySQL assigned it (yet)
+		if($newProfileId === null) {
+			$this->profileId = null;
+			return;
+		}
 
-	//verify the profile id is positive
-	if($newProfileId <= 0) {
-		throw(new \RangeException("Profile id must be a positive number."));
-	}
+		//verify the profile id is positive
+		if($newProfileId <= 0) {
+			throw(new \RangeException("Profile id must be a positive number."));
+		}
 
-	//convert and store the profile id
-	$this->profileId = intval($newProfileId);
+		//convert and store the profile id
+		$this->profileId = $newProfileId;
 }
 
 /**
@@ -147,7 +150,7 @@ public function setProfileId(int $newProfileId = null) {
  * @return string value of profileName
  **/
 public function getProfileName() {
-	return ($this->profileName);
+		return ($this->profileName);
 }
 
 /**
@@ -159,20 +162,20 @@ public function getProfileName() {
  * @throws \TypeError if $newProfileName is not a string
  **/
 public function setProfileName(string $newProfileName) {
-	//verify the profile's name is secure
-	$newProfileName = trim($newProfileName);
-	$newProfileName = filter_var($newProfileName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	if(empty($newProfileName) === true) {
-		throw(new \InvalidArgumentException("Profile name content is empty or insecure"));
-	}
+		//verify the profile's name is secure
+		$newProfileName = trim($newProfileName);
+		$newProfileName = filter_var($newProfileName, FILTER_SANITIZE_STRING);
+		if(empty($newProfileName) === true) {
+			throw(new \InvalidArgumentException("Profile name content is empty or insecure"));
+		}
 
-	//verify the profile name content will fit in the database
-	if(strlen($newProfileName) > 128) {
-		throw(new \RangeException("Name content too large"));
-	}
+		//verify the profile name content will fit in the database
+		if(strlen($newProfileName) > 128) {
+			throw(new \RangeException("Name content too large"));
+		}
 
-	//store the profile name
-	$this->profileName = $newProfileName;
+		//convert and store the profile name
+		$this->profileName = $newProfileName;
 }
 
 /**
@@ -180,7 +183,7 @@ public function setProfileName(string $newProfileName) {
  * @return string value of profileEmail
  **/
 public function getProfileEmail() {
-	return ($this->profileEmail);
+		return ($this->profileEmail);
 }
 
 /**
@@ -192,20 +195,20 @@ public function getProfileEmail() {
  * @throws \TypeError if $newProfileEmail is not a string
  **/
 public function setProfileEmail(string $newProfileEmail) {
-	//verify the profile's email content is secure
-	$newProfileEmail = trim($newProfileEmail);
-	$newProfileEmail = filter_var($newProfileEmail, FILTER_SANITIZE_EMAIL);
-	if(empty($newProfileEmail) === true) {
-		throw(new \InvalidArgumentException("Profile's email content is empty or insecure"));
-	}
+		//verify the profile's email content is secure
+		$newProfileEmail = trim($newProfileEmail);
+		$newProfileEmail = filter_var($newProfileEmail, FILTER_SANITIZE_EMAIL);
+		if(empty($newProfileEmail) === true) {
+			throw(new \InvalidArgumentException("Profile's email content is empty or insecure"));
+		}
 
-	//verify the email will fit in the database
-	if(strlen($newProfileEmail) > 128) {
-		throw(new \RangeException("Email too large"));
-	}
+		//verify the email will fit in the database
+		if(strlen($newProfileEmail) > 128) {
+			throw(new \RangeException("Email too large"));
+		}
 
-	//store the profile's email
-	$this->profileEmail = $newProfileEmail;
+		//convert and store the profile's email
+		$this->profileEmail = $newProfileEmail;
 }
 
 /**
@@ -213,7 +216,7 @@ public function setProfileEmail(string $newProfileEmail) {
  * @return string value of profileLocation
  **/
 public function getProfileLocation() {
-	return ($this->profileLocation);
+		return ($this->profileLocation);
 }
 
 /**
@@ -225,18 +228,20 @@ public function getProfileLocation() {
  * @throws \TypeError if $newProfileLocation is not a string
  **/
 public function setProfileLocation(string $newProfileLocation) {
-	//verify the profile's location content is secure
-	$newProfileLocation = trim($newProfileLocation);
-	$newProfileLocation = filter_var($newProfileLocation, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	if(empty($newProfileLocation) === true) {
-		throw (new \InvalidArgumentException("profile location is empty of insecure"));
-	}
-	if(strlen($newProfileLocation) > 64) {
-		throw (new \RangeException("profile location field is greater than 64 characters"));
-	}
+		//verify the profile's location content is secure
+		$newProfileLocation = trim($newProfileLocation);
+		$newProfileLocation = filter_var($newProfileLocation, FILTER_SANITIZE_STRING);
+		if(empty($newProfileLocation) === true) {
+			throw (new \InvalidArgumentException("profile location is empty of insecure"));
+		}
 
-	//store the profile's location
-	$this->profileLocation = $newProfileLocation;
+		//verify the email will fit in the database
+		if(strlen($newProfileLocation) > 64) {
+			throw (new \RangeException("profile location field is greater than 64 characters"));
+		}
+
+		//convert and store the profile's location
+		$this->profileLocation = $newProfileLocation;
 }
 
 /**
@@ -244,7 +249,7 @@ public function setProfileLocation(string $newProfileLocation) {
  * @return string value of profileBio
  **/
 public function getProfileBio() {
-	return ($this->profileBio)
+		return ($this->profileBio);
 }
 
 /**
@@ -253,27 +258,32 @@ public function getProfileBio() {
  * @param string $newProfileBio new value of profile location
  * @throws \InvalidArgumentException if $newProfileBio is not a string or is insecure
  * @throws \RangeException if $newProfileBio is > 255 characters
+ * @throws \TypeError if $newProfileBio is not a string
  **/
-public function setProfileBio($newProfileBio) {
-	//verify the profile bio content is secure
-	$newProfileBio = trim($newProfileBio);
-	$newProfileBio = filter_var($newProfileBio, FILTER_SANITIZE_STRING);
-	if(empty($newProfileBio) === true) {
-		throw (new \InvalidArgumentException("profile description is empty or insecure"));
-	}
-	if(strlen($newProfileBio) > 255) {
-		throw (new \RangeException("profile bio is greater than 255 characters"));
-	}
-	//store the profile bio content
-	$this->profileBio = $newProfileBio;
+public function setProfileBio(string $newProfileBio) {
+		//verify the profile bio content is secure
+		$newProfileBio = trim($newProfileBio);
+		$newProfileBio = filter_var($newProfileBio, FILTER_SANITIZE_STRING);
+		if(empty($newProfileBio) === true) {
+			throw (new \InvalidArgumentException("profile description is empty or insecure"));
+		}
+
+		//verify the profile bio will fit in the database
+		if(strlen($newProfileBio) > 255) {
+			throw (new \RangeException("profile bio is greater than 255 characters"));
+		}
+
+		//convert and store the profile bio content
+		$this->profileBio = $newProfileBio;
 }
 
 /**
  * accessor method for profileHash
- * @return int|null for $newProfileHash
+ * @return string $newProfileHash new value of profile hash
  **/
 public function getProfileHash() {
-	return ($this->profileHash);
+		unset($this->profileHash); //should not return hash
+		return ($this->profileHash);
 }
 
 /**
@@ -285,18 +295,19 @@ public function getProfileHash() {
  * @param \TypeError if $newProfileHash is not a string
  **/
 public function setProfileHash(string $newProfileHash) {
-	//verification that $profileHash is secure
-	$newProfileHash = strtolower(trim($newProfileHash));
-	//make sure that profile hash cannot be null
-	if(ctype_xdigit($newProfileHash) === false) {
-		throw(new \RangeException("profile hash cannot be null"));
-	}
-	//make sure profile hash = 128
-	if(strlen($newProfileHash) !== 128) {
-		throw(new \RangeException("profile hash has to be 128"));
-	}
-	//convert and store profile hash
-	$this->profileHash = $newProfileHash;
+		//verification that $profileHash is secure
+		$newProfileHash = strtolower(trim($newProfileHash));
+		$newProfileHash = filter_var($newProfileHash, FILTER_SANITIZE_STRING);
+		if(empty($newProfileHash) === true) {
+				throw(new \InvalidArgumentException("profile hash is empty or insecure"));
+		}
+		//verify the profile hash will fit into the database
+		if(strlen($newProfileHash) >128) {
+			throw(new \RangeException("profile hash too large"));
+		}
+
+		//convert and store profile hash
+		$this->profileHash = $newProfileHash;
 }
 
 /**
@@ -304,7 +315,8 @@ public function setProfileHash(string $newProfileHash) {
  * @return string value of profileSalt
  **/
 public function getProfileSalt() {
-	return ($this->profileSalt);
+		unset($this->profileSalt); //should not return salt
+		return ($this->profileSalt);
 }
 
 /**
@@ -316,17 +328,17 @@ public function getProfileSalt() {
  * @throws \TypeError if $newProfileSalt is not a string
  **/
 public function setProfileSalt(string $newProfileSalt) {
-	$newProfileSalt = strtolower(trim($newProfileSalt));
-	//verification that $profileSalt is secure
-	if(ctype_xdigit($newProfileSalt) === false) {
-		throw(new \RangeException("profile salt cannot be null"));
-	}
-	//make sure profile salt = 64
-	if(strlen($newProfileSalt) !== 64) {
-		throw(new \RangeException("profile has has to be 64"));
-	}
-	//convert and store profile salt
-	$this->profileSalt = $newProfileSalt;
+		$newProfileSalt = strtolower(trim($newProfileSalt));
+		//verification that $profileSalt is secure
+		if(empty($newProfileSalt) === true) {
+		throw(new \InvalidArgumentException("profile salt is empty or insecure"));
+		}
+		//make sure profile salt = 64
+		if(strlen($newProfileSalt) !== 64) {
+			throw(new \RangeException("profile has has to be 64"));
+		}
+		//convert and store profile salt
+		$this->profileSalt = $newProfileSalt;
 }
 
 /**
@@ -334,7 +346,7 @@ public function setProfileSalt(string $newProfileSalt) {
  * @return string value of profile access token
  **/
 public function getProfileAccessToken() {
-	return ($this->profileAccessToken)
+		return ($this->profileAccessToken);
 }
 
 /**
@@ -345,15 +357,15 @@ public function getProfileAccessToken() {
  * @throws \TypeError if $newProfileAccessToken is not a string
  **/
 public function setProfileAccessToken(string $newProfileAccessToken) {
-	//verify the profile Access Token is secure
-	$newProfileAccessToken = trim($newProfileAccessToken);
-	$newProfileAccessToken = filter_var($newProfileAccessToken, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	if(empty($newProfileAccessToken) === true) {
-		throw(new \InvalidArgumentException("Profile access token content is empty or insecure"));
-	}
+		//verify the profile Access Token is secure
+		$newProfileAccessToken = trim($newProfileAccessToken);
+		$newProfileAccessToken = filter_var($newProfileAccessToken, FILTER_SANITIZE_STRING);
+		if(empty($newProfileAccessToken) === true) {
+			throw(new \InvalidArgumentException("Profile access token content is empty or insecure"));
+		}
 
-	//convert store the profile Access Token
-	$this->profileAccessToken = $newProfileAccessToken;
+		//convert store the profile Access Token
+		$this->profileAccessToken = $newProfileAccessToken;
 }
 
 
@@ -362,7 +374,7 @@ public function setProfileAccessToken(string $newProfileAccessToken) {
  * @return string value of profile activation token
  **/
 public function getProfileActivationToken() {
-	return ($this->profileActivationToken);
+		return ($this->profileActivationToken);
 }
 
 /**
@@ -373,32 +385,30 @@ public function getProfileActivationToken() {
  * @throws \TypeError if $newProfileActivationToken is not a string
  **/
 public function setProfileActivationToken(string $newProfileActivationToken = null) {
-	//verify the profile activation token is secure
-	if($newProfileActivationToken === null) {
-		$this->profileActivationToken = null;
-		return;
-	}
+		//verify the profile activation token is secure
+		$newProfileActivationToken = trim($newProfileActivationToken);
+		$newProfileActivationToken = filter_var($newProfileActivationToken, FILTER_SANITIZE_STRING);
+		if(empty($newProfileActivationToken) === true) {
+			throw(new \InvalidArgumentException("Activation Token is empty or secure"));
+		}
 
-	$newProfileActivationToken = strtolower(trim($newProfileActivationToken));
-	if(ctype_xdigit($newProfileActivationToken) === false) {
-		throw(new \RangeException("profile activation token cannot be null"));
-	}
-	//make sure profile activation token = 32
-	if(strlen($newProfileActivationToken) !== 32) {
-		throw(new\RangeException("profile activation token has to be 32"));
-	}
+		//make sure profile activation token = 32
+		if(strlen($newProfileActivationToken) !== 32) {
+			throw(new\RangeException("profile activation token has to be 32"));
+		}
 
-	$this->profileActivationToken = $newProfileActivationToken;
+		//convert and store profile activation token
+		$this->profileActivationToken = $newProfileActivationToken;
 }
 
 /**
- * updates this profile in mySQL
+ * inserts this profile in mySQL
  *
  * @param \PDO $pdo PDO connection object
  * @throws \PDOException when mySQL related error occur
  * @throws \TypeError if $pdo is not a PDO connection object
  **/
-public function insert(/PDO $PDO) {
+public function insert(\PDO $PDO) {
 	//enforce the profileId id null (i.e., don't insert a profile that already exists)
 	if($this->profileId !== null) {
 		throw(new \PDOException("not a new profile"));
