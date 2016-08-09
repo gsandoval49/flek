43981fd79dff35c83a7edb329e4fc6268141d874
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-08 22:09:24 -0600

    completed accessor/mutators for favoritee and favoriter id https://app.asana.com/0/161552024814128/164443198012126

--- public_html/php/classes/Favorite.php
+++ public_html/php/classes/Favorite.php
@@ -29,22 +29,71 @@ class Favorite implements \JsonSerializable {
 	 * @throws \RangeException if data values are out of bounds
 	 * @throws \Exception is some other exception occurs
 	 */
-	public function__construct(int $newFavoriteeId = null, int $newFavoriterId = null) {
-		try {
-				$this->setFavoriteeId($newFavoriteeId);
-				$this->setFavoriterId($newFavoriterId);
-}		catch(\InvalidArgumentException $invalidArgumentException)
-{
-	//rethrow the exception to the caller
-	throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+public function__construct(int $newFavoriteeId = null, int $newFavoriterId = null) {
+try {
+$this->setFavoriteeId($newFavoriteeId);
+$this->setFavoriterId($newFavoriterId);
 }
+
+catch
+(\InvalidArgumentException $invalidArgumentException)
+	{
+		//rethrow the exception to the caller
+		throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+	}
 		catch(\RangeException $range) {
-			//rethrow the exception to the caller
+	//rethrow the exception to the caller
 	throw(new \RangeException($range->getMessage(), 0, $range));
 }
 		catch(\Exception $exception) {
-		//rethrow the exception to the caller
-			throw(new \Exception($exception->getMessage(), 0, $exception));
+	//rethrow the exception to the caller
+	throw(new \Exception($exception->getMessage(), 0, $exception));
+}
 		}
-
+		/*
+		 * accessor method for favoritee id
+		 * @return int|null value of favoritee id
+		 */
+		public function getFavoriteeId() {
+	return ($this->favoriteeId);
+}
+		/*
+		 * mutator method for favoritee id
+		 * @param int}null $newFavoriteeId new value of favoritee id
+		 * @throws \RangeException if $newFavoriteeId is not positive
+		 * @throws \Exception if $newFavoriteeId is not valid
+		 */
+		public function setFavoriteeId(int $newFavoriteeId = null) {
+	//if the favoritee id is null this a new favoritee given by sender
+	if($newFavoriteeId === null) {
+		$this->favoriteeId = null;
+		return;
+	}
+	//verify the favoritee id is positive
+	if($newFavoriteeId <= 0) {
+		throw(new \RangeException("favoritee Id is not positive"));
+	}
+	//convert and store the favoritee id
+	$this->favoriteeId = $newFavoriteeId;
+}
+		/*
+		 * accessor method for favoriter id
+		 * @return int|null value of favoriter id
+		 */
+		public function getFavoriterId() {
+	return ($this->favoriterId);
+}
+		/*
+		 * mutator method for favoriter id
+		 * @param int|null $newFavoriter new value of favoriter id
+		 * @throws \InvalidArgument if $newFavoriter is not valid
+		 */
+		public function setFavoriterId(int $newFavoriterId) {
+	//verify the favoriter id is true
+	if($newFavoriterId = false) {
+		throw(new \InvalidArgumentException("favoriter id is not true output"));
+	}
+	//convert and store the favoriter id
+	$this->favoriterId = $newFavoriterId;
+}
 
