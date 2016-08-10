187b20b695997340475095613b980c805bc17511
Robert Harding <robisonfire@gmail.com>
2016-08-10 15:18:19 -0600

    worked on MailTest.php https://app.asana.com/0/161552024814145/164443198012114

--- test/MailTest.php
+++ test/MailTest.php
@@ -1,7 +1,36 @@
 <?php
+
+namespace Edu\Cnm\Flek\Test;
+
+use Edu\Cnm\Flek\{Mail};
+//grab the project test parameters
+require_once ("MailTest.php");
+
+//grab the class under scrutiny
+
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
 /**
- * Created by PhpStorm.
- * User: robis_000
- * Date: 8/10/2016
- * Time: 2:36 PM
+ *full PHP unit test for the Mail class
+ *
+ * @author Rob Harding
+ * @ver 1.0.0
  */
+class MailTest extends FlekTest {
+	/**
+	 * this is the Mail content
+	 */
+	protected $VALID_MAILCONTENT = "PHP unit test passing";
+	/**
+	 * this is the updated Mail content
+	 */
+	protected $VALID_MAILCONTENT2 = "PHP unit test still passing";
+	/**
+	 * this is the profile who created/sent the message, this is for foreign key relations
+	 */
+	protected $sender = null;
+	/**
+	 * this is the profile who viewed/received the message, this is for foreign key relations
+	 */
+	protected $receiver = null;
+}
