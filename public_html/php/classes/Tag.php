e5b7eecc433ab7686261c13fc7667e7d132f078f
Robert Harding <robisonfire@gmail.com>
2016-08-09 23:26:47 -0600

    started Tag finished PDOs https://app.asana.com/0/161552024814145/164443198012124

--- public_html/php/classes/Tag.php
+++ public_html/php/classes/Tag.php
@@ -78,6 +78,146 @@ class Tag implements \JsonSerializable {
 			// update the null tagHashtagId with what mySQL just gave us
 			$this->tagHashtagId = intval($pdo->lastInsertId());
 		}
+
+
+		/*
+		 * deletes this favorite from my SQL
+		 * @param \PDO $pdo PDO connectin object
+		 * @throws \PDOException when mySQL related erros occur
+		 * @throws \TypeError if $pdo is not a PDO connection object
+		 */
+		public function delete(\PDO $pdo) {
+			//enforce the tage Id is not null
+			if($this->tagHashtagId === null) {
+				throw(new \PDOException("unable to delete a hashtag that does not exist"));
+			}
+			//create a query template
+			$query = "DELETE FROM Tag WHERE tagHashtagId = :tagHashtagId";
+			$statement = $pdo->prepare($query);
+
+			//bind the member variables to the place holder in the template
+			$parameters = ["tagHashtagId" => $this->tagHashtagId];
+			$statement->execute($parameters);
+		}
+
+		/*
+		 * updates this tag in mySQL
+		 * @param \PDO $pdo PDO connection object
+		 * @throws \PDOException when mySQL related erros occur
+		 * @throws \TypeError if $pdo is not a PDO connection object
+		 */
+		public function update(\PDO $pdo) {
+			//enforce the tagHashtagId is not null
+			if($this->tagHashtagId === null) {
+				throw(new \PDOException("unable to update a tag that does not exist"));
+			}
+
+			//create query template
+			$query = "UPDATE tag SET tagHashtagId = :tagHashtagId, tagImageId = :tagImageId";
+			$statement = $pdo->prepare($query);
+
+			//bind the member variables to the place holders in the template
+			$parameter = ["tagHashtagId" => $this->tagHashtagId, "tagImageId" => $this->tagImageId];
+			$statement->execute($parameter);
+		}
+		/*
+		 * gets the tag by tagHashtagId
+		 * @param \PDO $pdo PDO connection object
+		 * @param int $tagHashtagId tagHashtag id to search for
+		 * @return tag|null tag found or null if not found
+		 * @throws \PDOException when mySQL related erros occur
+		 * @throws |TypeError when variables are not the correct data type
+		 */
+		public static function getTagBytagHashtagId(\PDO $pdo, int $tagHashtagId) {
+			//sanitize the tagHashtagId before searching
+			if($tagHashtagId <= 0) {
+				throw(new \PDOException("tagHashtag id is not positive"));
+			}
+			// create query templatte
+			$query = "SELECT tagHashtagId, tagImageId FROM tag WHERE tagHashtagId = :tagHashtagId";
+			$statement = $pdo->prepare($query);
+
+			//bind the tagHashtag Id to theplace holder in teh template
+			$parameters = ["tagHashtagId => $tagHashtagId"];
+			$statement->execute($parameters);
+
+			//grab the tag from mySQL
+			try {
+				$tag = null;
+				$statement->setFetchMode(\PDO::FETCH_ASSOC);
+				$row = $statement->fetch();
+				if($row !== false) {
+					$tag = new Tag ($row["tagHashtagId"], $row["tagImageId"]);
+				}
+			} catch(\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+			return ($tag);
+		}
+
+		/* gets the tag by tagImageId
+	  * @param \PDO $pdo PDO connection object
+	  * @param int $tagImageId tagHashtag id to search for
+			* @return tag|null tag found or null if not found
+	  * @throws \PDOException when mySQL related erros occur
+	  * @throws |TypeError when variables are not the correct data type
+	  */
+		public static function getTagByTagImageId(\PDO $pdo, int $tagImageId) {
+			//sanitize the tagHashtagId before searching
+			if($tagImageId <= 0) {
+				throw(new \PDOException("tagHashtag id is not positive"));
+			}
+			// create query templatte
+			$query = "SELECT tagHashtagId, tagImageId FROM tag WHERE tagImageId = :tagImageId";
+			$statement = $pdo->prepare($query);
+
+			//bind the tagHashtag Id to theplace holder in teh template
+			$parameters = ["tagImageId => $tagImageId"];
+			$statement->execute($parameters);
+
+			//grab the tag from mySQL
+			try {
+				$tag = null;
+				$statement->setFetchMode(\PDO::FETCH_ASSOC);
+				$row = $statement->fetch();
+				if($row !== false) {
+					$tag = new Tag ($row["tagHashtagId"], $row["tagImageId"]);
+				}
+			} catch(\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+			return ($tag);
+		}
+
+		/*
+		 * gets all tags
+		 * @param \SplFixedArray SplFixedArray of tags found or null if not found
+		 * @throws \PDOException when mySQL related errors occur
+		 * @throws \TypeError when variables are not the correct data type
+		 */
+		public static function getAllTags(\PDO $pdo) {
+			//create query template
+			$query = "SELECT tagHashtagId, tagImageId FROM tag";
+			$statement = $pdo->prepare($query);
+			$statement->execute();
+
+			//build an array of tags
+			$tags = new \SplFixedArray($statement->rowCount());
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			while(($row = $statement->fetch()) !== false) {
+				try {
+					$tags = new Tag($row["tageId"], $row["tagrId"]);
+					$tags[$tag->key()] = $tag;
+					$tags->next();
+				} catch(\Exception $exception) {
+					//if the row couldn't be converted rethrow it
+					throw(new \PDOException($exception->getMessage(), 0, $exception));
+				}
+			}
+			return ($tags);
+		}
 	/**
 	 * formats state variables for JSON serialization
 	 *
