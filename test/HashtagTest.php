ca63a3d1a0c2a7cde5d72fa360cdaf2be5d1d6c6
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-11 15:58:54 -0600

    added hashtag doc block and beginning class. https://app.asana.com/0/161555263951762/164443198012116

---
+++ test/HashtagTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Edu\Cnm\Flek\Test;
+
+use Edu\Cnm\Flek\{Hashtag};
+use PDOException;
+
+//grab the project test parameters
+require_once("HashtagTest.php");
+
+// grab the class that's going through the x-ray and under the knife :)
+require_once (dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Hashtag class
+ *
+ * This is a complete test of the Hashtag class. It is complete because ALL mySQL/PDO enabled methods are tested for both invalid and valid inputs
+ *
+ * @see Hashtag
+ * @author Giles Sandoval <gsandoval49@cnm.edu>
+ **/
+class HashtagTest extends FlekTest {
+    /**
+     *
+     **/
+}
