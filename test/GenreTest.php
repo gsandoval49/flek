de6b6ab616702525c848f49c435ed67997291667
Christina Sosa <csosa2106@gmail.com>
2016-08-11 15:47:41 -0600

    ProfileTest- created genre test file https://app.asana.com/0/161549255700615/164443198012120

---
+++ test/GenreTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Edu\Cnm\Flek\Test;
+
+use Edu\Cnm\Flek\{Genre};
+
+//grab teh project test parameters
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Genre class
+ *
+ * This is a complete PHPUnit test of the genre class. It is complete because *ALL* mySQL/PDO enabled methods are
+ * tested for both invalid and valid inputs.
+ *
+ * @see Genre
+ * @author Christina Sosa <csosa4@cnm.edu>
+ **/
