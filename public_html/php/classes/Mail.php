3e9f5e3bb2c8ad5e377c585e3fc296db8199bf8f
Robert Harding <robisonfire@gmail.com>
2016-08-12 09:32:08 -0600

    updating Mail.php to have "mailDateTime" https://app.asana.com/0/161552024814145/164443198012114

--- public_html/php/classes/Mail.php
+++ public_html/php/classes/Mail.php
@@ -290,7 +290,7 @@ public static function getMessageByMessageContent(\PDO $pdo, string $mailContent
 		throw(new\PDOException("message content is invalid"));
 	}
 	/*create query template*/
-	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent FROM mail WHERE mailContent LIKE :mailContent";
+	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent, mailDateTime FROM mail WHERE mailContent LIKE :mailContent";
 	$statement = $pdo->prepare($query);
 
 	/*bind the message content to the placeholder*/
@@ -303,7 +303,7 @@ public static function getMessageByMessageContent(\PDO $pdo, string $mailContent
 	$statement->setFetchMode(\PDO::FETCH_ASSOC);
 	while(($row = $statement->fetch()) !== false){
 		try{
-			$mail = new Mail($row["mailId"], $row["mailSubject"], $row ["mailSenderId"], $row ["mailReceiverId"], $row ["mailGunId"], $row["mailContent"]);
+			$mail = new Mail($row["mailId"], $row["mailSubject"], $row ["mailSenderId"], $row ["mailReceiverId"], $row ["mailGunId"], $row["mailContent"], $row["mailDateTime"]);
 			$messages[$messages->key()] = $mail;
 			$messages->next();
 		} catch(\Exception $exception){
@@ -324,7 +324,7 @@ public static function getMailByMailId(\PDO $pdo, int $mailId){
 		throw(new\PDOException("message Id is not positive"));
 	}
 	/*create query template*/
-	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent FROM mail WHERE mailId = :mailId";
+	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent, mailDateTime FROM mail WHERE mailId = :mailId";
 	$statement = $pdo->prepare($query);
 
 	/*bind mailId to the placeholder in template*/
@@ -336,7 +336,7 @@ public static function getMailByMailId(\PDO $pdo, int $mailId){
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		$row =  $statement->fetch();
 		if($row !== false){
-			$mail = new Mail($row["mailId"], $row["mailSubject"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"]);}
+			$mail = new Mail($row["mailId"], $row["mailSubject"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"], $row["mailDateTime"]);}
 		} catch (\Exception $exception) {
 			/*rethrow the row if you can't convert it*/
 			throw(new\PDOException($exception->getMessage(),0, $exception));
@@ -355,7 +355,7 @@ public static function getMailByMailSenderId (\PDO $pdo, int $mailSenderId){
 		throw(new\RangeException("message sender profile Id must be positive"));
 	}
 	/*create query template*/
-	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent FROM mail WHERE mailSenderId = :mailSenderId";
+	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent, mailDateTime FROM mail WHERE mailSenderId = :mailSenderId";
 	$statement = $pdo->prepare($query);
 	/*bind the Sender Id to the placeholder template*/
 	$parameters = ["mailSenderId" => $mailSenderId];
@@ -365,7 +365,7 @@ public static function getMailByMailSenderId (\PDO $pdo, int $mailSenderId){
 	$statement->setFetchMode(\PDO::FETCH_ASSOC);
 	while(($row = $statement->fetch()) !== false){
 		try{
-			$mail = new Mail($row["mailId"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"]);
+			$mail = new Mail($row["mailId"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"], $row["mailDateTime"]);
 			/*I'm not so sure about this one, is this the Id from profile??*/
 			$messages[$messages->key()] = $mail;
 			$messages->next();
@@ -387,7 +387,7 @@ public static function getMailByMailReceiverId (\PDO $pdo, int $mailReceiverId){
 		throw(new\RangeException("message Receiver profile Id must be positive"));
 	}
 	/*create query template*/
-	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent FROM mail WHERE mailReceiverId = :mailReceiverId";
+	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent, mailDateTime FROM mail WHERE mailReceiverId = :mailReceiverId";
 	$statement = $pdo->prepare($query);
 	/*bind the Receiver Id to the placeholder template*/
 	$parameters = ["mailReceiverId" => $mailReceiverId];
@@ -397,7 +397,7 @@ public static function getMailByMailReceiverId (\PDO $pdo, int $mailReceiverId){
 	$statement->setFetchMode(\PDO::FETCH_ASSOC);
 	while(($row = $statement->fetch()) !== false){
 		try{
-			$mail = new Mail($row["mailId"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"]);
+			$mail = new Mail($row["mailId"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"], $row["mailDateTime"], $row["mailDateTime"]);
 			/* really not so sure about this one... is this the Id from profile??*/
 			$messages[$messages->key()] = $mail;
 			$messages->next();
@@ -414,7 +414,7 @@ return($messages);
  * */
 public static function getAllMessages(\PDO $pdo){
 	/*create query template*/
-	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent FROM mail";
+	$query = "SELECT mailId, mailSubject, mailSenderId, mailReceiverId, mailGunId, mailContent, mailDateTime FROM mail";
 	$statement = $pdo->prepare($query);
 	$statement->execute();
 	/*build an array of messages*/
@@ -422,7 +422,7 @@ public static function getAllMessages(\PDO $pdo){
 	$statement->setFetchMode(\PDO::FETCH_ASSOC);
 	while(($row = $statement->fetch()) !== false){
 		try{
-			$mail = new Mail($row["mailId"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"]);
+			$mail = new Mail($row["mailId"], $row["mailSenderId"], $row["mailReceiverId"], $row["mailGunId"], $row["mailContent"], $row["mailDateTime"]);
 			$messages[$messages->key()] = $mail;
 			$messages->next();
 		} catch(\Exception $exception){
