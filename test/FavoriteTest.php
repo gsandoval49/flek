a0d2d2d13842bd4e19a21132fe5c6e03f262dfff
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-12 09:56:31 -0600

    set up comments for my classes for favorite https://app.asana.com/0/161552024814128/164443198012126

--- test/FavoriteTest.php
+++ test/FavoriteTest.php
@@ -11,4 +11,24 @@ require_once (dirname(__DIR__) . "/public_html/php/classes/autoload.php");
 
 /*
  * Full PHPUnit test for the Favorite Class
+ *
+ * This is a complete PHPUnit test of the Favorite class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Favorite
+ * @author Diane Peshlakai <dpeshlakai3@cnm.edu>
  */
+
+class FavoriteTest extends FlekTest {
+	/*
+	 * Profile that created Favoritee and Favoriter
+	 *@var profile profile for Favorite
+	 */
+	protected $ = null;
+
+	/*
+	 * profile that created Favoriter
+	 * @var profile for Favorite
+	 */
+	protected $=
+	}
