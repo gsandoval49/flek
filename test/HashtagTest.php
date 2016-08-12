abe41dd7cb0ef46b44c5a1dac43feb1ff469d2fb
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-12 15:07:59 -0600

    added test Insert valid and invalid & test update valid and invalid & test delete invalid https://app.asana.com/0/161552024814134/164443198012122

--- test/HashtagTest.php
+++ test/HashtagTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Edu\Cnm\Flek\Test;
 
-use Edu\Cnm\Flek\{Hashtag};
+use Edu\Cnm\Flek\{
+    Hashtag, Tag
+};
 use PDOException;
 
 //grab the project test parameters
@@ -20,28 +22,123 @@ require_once (dirname(__DIR__) . "/public_html/php/classes/autoload.php");
  **/
 class HashtagTest extends FlekTest {
     /**
-     * Hashtag name created by profile.
-     * @var string $VALID_HASHTAG
+     * Hashtag content for the tag.
+     * @var string $VALID_HASHTAG_CONTENT
      **/
-    protected $VALID_HASHTAG = "This better work";
+    protected $VALID_HASHTAG_CONTENT = "This better work";
+    /**
+     * content of the updated hashtag
+     * @var string $VALID_HASHTAG_CONTENT2
+     **/
+    protected %VALID_HASHTAG_CONTENT2 = "This part better work too!"
     /**
      * Test inserting a valid hashtag and verifying that mySQL data matches
      **/
-
-
     public function testInsertValidHashtag() {
-        // Count the number of rows and save it for later later
+         // NO DEPENDENT OBJECTS - didn't create dependent objects code aka setUp
+         // count the number of rows and save it for later later
         $numRows = $this->getConnection()->getRowCount("hashtag");
 
-        // Create a new hashtag and insert it into mySQL
-        $hashtag = new Hashtag(null, $this->VALID_HASHTAG_LABEL);
+        // create a new Hashtag and insert into mySQL
+        $hashtag = new Hashtag(null, $this->VALID_HASHTAG_CONTENT);
+        $hashtag->insert($this->getPDO());
+        // grab the data from mySQL and enforce the fields match
+
+        $pdoHashtag = Hashtag::getHashtagByHashtagId($this->getPDO(), $hashtag->getHashtagId());
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("hashtag"));
+        $this->assertEquals($pdoHashtag->getHashtagName($this->VALID_HASHTAG_CONTENT), $this->VALID_HASHTAG_CONTENT); //do i need valid hashtag content twice?
+    }
+
+    /**
+     * test inserting a Hashtag that already exists
+     *
+     * @expectedException PDOException
+     **/
+    public function testInsertInvalidHashtag() {
+        $hashtag = new Hashtag(FlekTest::INVALID_KEY, $this->VALID_HASHTAG_CONTENT);
+        $hashtag->insert($this->getPDO());
+    }
+
+    /**
+     * test inserting a Hashtag, editing it, and then updating it
+     **/
+    public function testUpdateValidHashtag() {
+        // count the number of rows and save it for later
+        $numRows = $this->getConnection()->getRowCount("hashtag");
+
+        // create a new Hashtag and insert into mySQL
+        $hashtag = new Hashtag(null, $this->VALID_HASHTAG_CONTENT);
+
+        // edit the Hashtag and update it in mySQL
+        $hashtag->setHashtagName($this->VALID_HASHTAG_CONTENT2);
+        $hashtag->update($this->getPDO());
+
+        // grab the data from mySQL and enforce the fields to match our expectations
+        $pdoHashtag = Hashtag::getHashtagByHashtagId($this->getPDO(), $hashtag->getHashtagId());
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("hashtag"));
+        $this->assertEquals($pdoHashtag->getHashtagName(), $this->VALID_HASHTAG_CONTENT2);
+    }
+
+    /**
+     * test updating a Hashtag and then deleting it
+     **/
+    public function testDeleteValidHashtag() {
+        //count the number of rows and save it for later
+        $numRows = $this->getConnection()->getRowCount("hashtag");
+
+        // create a new Hashtag and insert into mySQL
+        $hashtag = new Hashtag(null, $this->VALID_HASHTAG_CONTENT);
         $hashtag->insert($this->getPDO());
 
-        // Grab the data from mySQL and check the fields against our expectations
-        $pdoHashtag = Hashtag::getHashtagByHashtagName($this->getPDO(), $hashtag->getHashtagName());
+        // delete the Hashtag from mySQL
         $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("hashtag"));
-        $this->assertEquals($pdoHashtag->getHashtagLabel(), $this->VALID_HASHTAG);
+        $hashtag->delete($this0>$this->getPDO());
+
+        // grab the data from mySQL and enforce the Hashtag does not exist
+        $pdoHashtag = Hashtag::getHashtagByHashtagId($this->getPDO(), $hashtag->getHashtagId());
+        $this->assertNull($pdoHashtag);
+        $this->assertEquals($numRows, $this->getConnection()->getRowCount("hashtag"));
     }
+
+    /**
+     * test deleting a Hashtag that does NOT exist
+     *
+     * @expectedException PDOException
+     **/
+    public function testDeleteInvalidHashtag() {
+        //create a Hashtag and try to delete it without actually inserting it
+        $hashtag = new Hashtag(null, $this->VALID_HASHTAG_CONTENT);
+        $hashtag->delete($this->getPDO());
+    }
+
+    /**
+     * test grabbing a Hashtag by Hashtag content
+     **/
+    public function testGetValidHashtagByHashtagContent() {
+        // count the number of rows and save it for later
+        $numRows = $this->getConnection()->getRowCount("hashtag");
+
+        // create a new Hashtag and insert to into mySQL
+        $hashtag = new Hashtag(null, $this->VALID_HASHTAG_CONTENT);
+        $hashtag->insert($this->getPDO());
+
+        // grab the data from mySQL and enforce the fields match our expectations
+        $results = Hashtag::getHashtagByHashtagName($this->getPDO(), $hashtag->getHashtagName());
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("hashtag"));
+        $this->assertCount(1, $results);
+
+        // grab the result from the array and validate it
+        $pdoHashtag = $results[0];
+        $this->assertEquals($pdoHashtag->getHashtagName(), $this->VALID_HASHTAG_CONTENT);
+    }
+
+    /**
+     * test grabbing a Hashtag that does NOT exist
+     **/
+    
+
+
+
 
 
 }
