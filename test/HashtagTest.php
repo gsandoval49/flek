c2c07c562efecf43b7f135806ae999ac9b227189
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-12 15:58:19 -0600

    added get all valid tags https://app.asana.com/0/161552024814134/164443198012122

--- test/HashtagTest.php
+++ test/HashtagTest.php
@@ -150,6 +150,31 @@ class HashtagTest extends FlekTest {
         $this->assertCount(0, $hashtag);
     }
 
+    /**
+     * test grabbing all Tags
+     **/
+    public function testGetAllValidHashtags() {
+        // count the number of rows and save it for later
+        $numRows = $this->getConnection()->getRowCount("hashtag");
+
+        // create a new Hashtag and insert into mySQL
+        $hashtag = new Tag(null, $this->VALID_HASHTAG_CONTENT);
+        $hashtag->insert($this->getPDO());
+
+        //grab the data from mySQL and enforce the fields match our expectations
+        $results = Hashtag::getAllHashtags($this->getPDO());
+        $this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("hashtag"));
+        $this->assertCount(1, $results);
+
+        // grab the result from the array and validate it
+        $pdoHashtag = $results[0];
+        $this->assertEquals($pdoHashtag->getHashtagName(), $this->VALID_HASHTAG_CONTENT);
+    }
+
+    /**
+     *
+     **/
+
 
     
 
