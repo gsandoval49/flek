fdb249baff88f87ec2056706a1249008c2490204
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-11 22:28:15 -0600

    commit new file for Favorite class   https://app.asana.com/0/161552024814128/164443198012118

---
+++ test/FavoriteTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Edu\Cnm\DataDesign\Test;
+
+use Edu\Cnm\Flek\DataDesign\Favorite;
+
+//grab the project test parametes
+require_once("FlekTest.php");
+
+//grab the class
+require_once (dirname(__DIR__) . "/php/classes/autoload.php");
+
+/*
+ * Full PHPUnit test for the Favorite Class
+ */
