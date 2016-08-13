07757122585bce2feff79f73e1509e0921290cd9
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-13 10:30:38 -0600

    setted up the favoriter and favoritee to own the test https://app.asana.com/0/161552024814128/164443198012126

--- test/FavoriteTest.php
+++ test/FavoriteTest.php
@@ -24,11 +24,37 @@ class FavoriteTest extends FlekTest {
 	 * Profile that created Favoritee and Favoriter
 	 *@var profile profile for Favorite
 	 */
-	protected $ = null;
+	protected $favoritee = null;
 
 	/*
 	 * profile that created Favoriter
 	 * @var profile for Favorite
 	 */
-	protected $=
+	protected $favoriter = null;
 	}
+
+	/*
+	 * create dependent objects before running each test
+	 */
+	public final function setUp() {
+		// run the deafault setUp() method first
+		parent::setUp();
+		// create and insert a Favoritee to own the test Favorite
+		$this->favoritee = new Favoritee(null, "It's going to rain today", "test@phpunit.de", "here in the land of enchantment
+		its 77 degrees right now");
+		$this->favoritee->insert($this->getPDO());
+
+		//create and insert a Favoriter to own the test Favorite
+		$this->favoriter = new Favoriter(null, "I cant wait to see the Star wars movie", "508 warehouse is doing an event today");
+		$this->favoriter->insert($this->getPDO());
+}
+	/*
+	 * test inerting a valid Favorite and verify that the actual mySQL data matches
+	 */
+		public function testInsertValidFavorite() {
+			//count the number of rows and save it for later
+			$numRows = $this->getConnection()->getRowCount("Favorite");
+
+		//create a new Favorite and insert to into mySQL
+		$favorite = new Favorit
+}
