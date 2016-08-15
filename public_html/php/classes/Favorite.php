4cf46dcbca2682b76a65d300c60cfea1d8bab24d
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-15 08:53:05 -0600

    fixed typos and added type error to throws exception on line 32 https://app.asana.com/0/161555263951762/164443198012126

--- public_html/php/classes/Favorite.php
+++ public_html/php/classes/Favorite.php
@@ -2,48 +2,53 @@
 namespace Edu\Cnm\Flek;
 
 require_once("autoload.php");
-/*
+/**
  * This favorite can be considered a small example of what could be stored when profiles
  *
  * @author Diane Peshlakai <dpeshlakai3@cnm.edu>
  * @version 1.0.0
- */
+ **/
 class Favorite implements \JsonSerializable {
-	/*
+	/**
 	 * id for Favorite when profile can favorite the profile and identify the profile
 	 *  this is the foreign key
 	 * @var int $favoriteeId
-	 */
+	 **/
 	private $favoriteeId;
-	/*
+	/**
 	 * int $favoriterId
 	 * id for the favorite the profile can favorite vice versa
 	 * this is a foreign key
-	 */
+	 **/
 	private $favoriterId;
 
-	/*
+	/**
 	 * constructor for favorite
-	 *@param int|null $newFavoritee
-	 *@param int|null $newFavoriter
+	 * @param int|null $newFavoritee
+     * @param int|null $newFavoriter
+     * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds
 	 * @throws \Exception is some other exception occurs
-	 */
+     * @throws \TypeError if data types violate type hints
+	 **/
 	public function __construct(int $newFavoriteeId = null, int $newFavoriterId = null) {
-		try {
-			$this->setFavoriteeId($newFavoriteeId);
-			$this->setFavoriterId($newFavoriterId);
-		} catch
-		(\InvalidArgumentException $invalidArgumentException) {
-			//rethrow the exception to the caller
-			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
-		} catch(\RangeException $range) {
-			//rethrow the exception to the caller
-			throw(new \RangeException($range->getMessage(), 0, $range));
-		} catch(\Exception $exception) {
-			//rethrow the exception to the caller
-			throw(new \Exception($exception->getMessage(), 0, $exception));
-		}
+        try {
+            $this->setFavoriteeId($newFavoriteeId);
+            $this->setFavoriterId($newFavoriterId);
+        } catch (\InvalidArgumentException $invalidArgument) {
+            //rethrow the exception to the caller
+            throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+        } catch (\RangeException $range) {
+            //rethrow exception to caller
+            throw(new \RangeException($range->getMessage(), 0, $range));
+        } catch (\TypeError $typeError) {
+            //rethrow exception to caller
+            throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+        } catch (\Exception $exception) {
+            //rethrow regular exception to caller
+            throw(new \Exception($exception->getMessage(), 0, $exception));
+        }
+    }
 
 		/*
 		 * accessor method for favoritee id
@@ -59,8 +64,7 @@ class Favorite implements \JsonSerializable {
 		 * @throws \RangeException if $newFavoriteeId is not positive
 		 * @throws \Exception if $newFavoriteeId is not valid
 		 */
-		public
-		function setFavoriteeId(int $newFavoriteeId = null) {
+		public function setFavoriteeId(int $newFavoriteeId = null) {
 			//if the favoritee id is null this a new favoritee given by sender
 			if($newFavoriteeId === null) {
 				$this->favoriteeId = null;
