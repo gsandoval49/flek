1955b27d29797c846ab2f52f89d044857f7035d8
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-15 09:47:00 -0600

    added and fixed line 4 https://app.asana.com/0/161552024814128/164443198012126

--- test/FavoriteTest.php
+++ test/FavoriteTest.php
@@ -1,11 +1,10 @@
 <?php
 namespace Edu\Cnm\Flek\Test;
 
-use Edu\Cnm\Flek\{
-	Favorite
-};
+use Edu\Cnm\Flek\{Favorite};
 
 //grab the project test parameters
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
 require_once("FlekTest.php");
 
 //grab the class
