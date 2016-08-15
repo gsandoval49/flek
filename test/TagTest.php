71492f0243aedef0176cf2a129481ca945e8c41b
Robert Harding <robisonfire@gmail.com>
2016-08-15 14:32:24 -0600

    TagTest still needs work https://app.asana.com/0/161552024814145/164443198012124

--- test/TagTest.php
+++ test/TagTest.php
@@ -11,9 +11,11 @@ require_once("FlekTest.php");
 
 class TagTest extends FlekTest {
 
-//Id from the image
+	//Id of the tag itself
+	protected $tagId;
+	//Id from the image
 	protected $tagImageId;
-//Id from the hashtag
+	//Id from the hashtag
 	protected $tagHashtagId;
 
 
@@ -24,9 +26,136 @@ class TagTest extends FlekTest {
 		$this->hashtag = new Hashtag(null, "booya", "content");
 		$this->hashtag->insert($this->getPDO());
 		// create and insert an Image to own the test Tag
-		$this->image = new Image(null, "filename", "image/jpg");
+		$this->image = new Image(null, "filename", "image");
 		$this->image->insert($this->getPDO());
+	}
+	public function testInsertValidTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null,$this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTag = Tag::getTagByTagId($this->getPDO(), $tag->getTagId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertEquals($pdoTag->getTagName($this->tagImageId), $this->tagHashtagId);
+	}
+	/**
+	 * test inserting a Tag that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testInsertInvalidTag() {
+		// create a Tag with a non null tag id and watch it fail
+		$tag = new Tag(DevConnectTest::INVALID_KEY, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+	}
+	/**
+	 * test inserting a Tag editing it and then updating it
+	 **/
+	public function testUpdateValidTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// edit the Tag and update it in mySQL
+		$tag->setTagName($this->tagImageId, $this->tagHashtagId2);
+		$tag->update($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTag = Tag::getTagByTagId($this->getPDO(), $tag->getTagId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertEquals($pdoTag->getTagName(), $this->tagImageId, $this->tagHashtagId2);
+	}
+	/**
+	 * test updating a Tag that does not exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testUpdateInvalidTag() {
+		// create a Tag with a non null tag id and watch it fail
+		$tag = new Tag(null,  $this->tagImageId, $this->tagHashtagId);
+		$tag->update($this->getPDO());
+	}
+	/**
+	 * test creating a Tag then deleting it
+	 **/
+	public function testDeleteValidTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// delete the Tag from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$tag->delete($this->getPDO());
+		// grab the data from mySQL and enforce the Tag does not exist
+		$pdoTag = Tag::getTagByTagId($this->getPDO(), $tag->getTagId());
+		$this->assertNull($pdoTag);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("tag"));
+	}
+	/**
+	 * test deleting a Tag that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testDeleteInvalidTag() {
+		// create a Tag and try to delete it without actually inserting it
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->delete($this->getPDO());
+	}
+	/**
+	 * test grabbing a Tag by tag content
+	 **/
+	public function testGetValidTagByTagContent() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tag::getTagByTagContent($this->getPDO(), $tag->getTagName());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertCount(1, $results);
+		// grab the result from the array and validate it
+		$pdoTag = $results[0];
+		$this->assertEquals($pdoTag->getTagName(), $this->tagImageId, $this->tagHashtagId);
+	}
+	/**
+	 * test grabbing a Tag that does not exist
+	 **/
+	public function testGetInvalidTagByTagId() {
+		// grab a profile id that exceeds the maximum allowable profile id
+		$tag = Tag::getTagByTagId($this->getPDO(), DevConnectTest::INVALID_KEY);
+		$this->assertNull($tag);
+	}
+	/**
+	 * test grabbing a Tag by content that does not exist  DYLAN DIDN'T DO THIS!!!!
+	 **/
+	public function testGetInvalidTagByTagContent() {
+		// grab a tag by content that does not exist
+		$tag = Tag::getTagByTagContent($this->getPDO(), "nobody ever made this TAG");
+		$this->assertCount(0, $tag);
+	}
+	/**
+	 * test grabbing all Tags
+	 **/
+	public function testGetAllValidTags() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tag::getAllTags($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertCount(1, $results);
+		// grab the result from the array and validate it
+		$pdoTag = $results[0];
+		$this->assertEquals($pdoTag->getTagName(), $this->tagImageId, $this->tagHashtagId);
+	}
+
 	}
 
 
-}
+
