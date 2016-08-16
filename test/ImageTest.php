32ab98c5c1fdfd59ff9335e9ff7180f9058c61b0
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-08-15 19:34:57 -0600

    edited profile class set up and added another quote https://app.asana.com/0/161552024814128/164443198012118

--- test/ImageTest.php
+++ test/ImageTest.php
@@ -70,7 +70,7 @@ class ImageTest extends FlekTest {
 */
 
 		//create a user that owns the image
-		$this->profile = new Profile(null, "Arlene", "bar@foo.com", "foo@bar.com", "Taos, NM", "so many type of art to see", "local artists in the state of new mexico", "art lovers are welcome");
+		$this->profile = new Profile(null, "Arlene", "bar@foo.com", "foo@bar.com", "Taos, NM", "so many type of art to see", "local artists in the state of new mexico", "art lovers are welcome", "test is still passing");
 		$this->profile->insert($this->getPDO());
 
 }
