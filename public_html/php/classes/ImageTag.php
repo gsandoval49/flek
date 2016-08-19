28e957bd3214c83404da2021a4fd139b55016988
Christina Sosa <csosa2106@gmail.com>
2016-08-18 20:47:35 -0600

    created ImageTag class and test files and transferred code over to new files https://app.asana.com/0/161555263951762/164443198012124

---
+++ public_html/php/classes/ImageTag.php
@@ -0,0 +1,290 @@
+<?php
+
+namespace Edu\Cnm\Flek;
+
+
+require_once("autoload.php");
+
+/**
+ * @author Rob Harding
+ * @ver 1.0.0
+ **/
+
+
+class Tag implements \JsonSerializable {
+	/**
+	 * id for image; this is a foreign key
+	 * @var int $tagImageId
+	 **/
+	private $tagImageId;
+	/**
+	 * id for hashtag; this is a foreign key
+	 * @var int $tagHashtagId
+	 **/
+
+	private $tagHashtagId;
+
+	/**
+	 * @param int|null $newTagImageId id of this image or null if a new image
+	 * @param int|null $newTagHashtagId id of this hashtag or null if new image
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 **/
+
+	public function __construct(int $newTagImageId = null, int $newTagHashtagId = null) {
+		try {
+			$this->setTagImageId($newTagImageId);
+			$this->setTagHashtagId($newTagHashtagId);
+		} catch(\InvalidArgumentException $InvalidArgument) {
+			//rethrow the exception to the caller
+			throw(new \InvalidArgumentException($InvalidArgument->getMessage(), 0, $InvalidArgument));
+		} catch(\RangeException $range) {
+			//rethrow the exception to the caller
+			throw (new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			//rethrow exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\ Exception $exception) {
+			//rethrow the exception to the caller
+			throw (new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * accessor method for the tagImageId
+	 *
+	 * @return int|null value of tag image id
+	 **/
+	public function getTagImageId() {
+		return ($this->tagImageId);
+	}
+
+	/**
+	 * mutator method for the tagImageId
+	 *
+	 * @param int|null $newTagImageId new value of tag image id
+	 * @throws \RangeException if $newTagImageId is not positive
+	 * @throws \TypeError if $newTagImageId is not an integer
+	 **/
+	public function setTagImageId(int $newTagImageId = null) {
+		//verify the tweet id is positive
+		if($newTagImageId <= 0) {
+			throw(new \RangeException ("image id is not positive"));
+		}
+		//convert and store the image id*
+		$this->tagImageId = $newTagImageId;
+	}
+
+	/**
+	 * accessor method for tagHashtagId
+	 *
+	 * @return int value of tag hashtag id
+	 **/
+	public function getTagHashtagId() {
+		return ($this->tagHashtagId);
+	}
+
+	/**
+	 * mutator method for tagHashtagId
+	 *
+	 * @param int $newTagHashtagId new value of tag hashtag id
+	 * @throws \RangeException if $newTagHashtagId is not positive
+	 * @throws \TypeError if $newTagHashtagId is not an integer
+	 **/
+	public function setTagHashtagId(int $newTagHashtagId) {
+		//verify the porfile id is positive
+		if($newTagHashtagId <= 0) {
+			throw(new \RangeException ("hashtag id is not positive"));
+		}
+		//convert and store the hashtag id
+		$this->tagHashtagId = $newTagHashtagId;
+	}
+
+	/**
+	 * inserts this Tag into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo) {
+		//check tag exists before entering into SQL
+		if($this->tagImageId === null || $this->tagHashtagId === null) {
+			throw(new \PDOException("not a valid tag"));
+		}
+		//create query template
+		$query = "INSERT INTO Tag(tagImageId, tagHashtagId) VALUES(:tagImageId, :tagHashtagId)";
+		$statement = $pdo->prepare($query);
+		//bind the member variables to the place holders in the template
+		$parameters = ["tagHashtagId" => $this->tagHashtagId, "tagImageId" => $this->tagImageId];
+		$statement->execute($parameters);
+	}
+
+
+	/**
+	 * deletes this favorite from my SQL
+	 *
+	 * @param \PDO $pdo PDO connectin object
+	 * @throws \PDOException when mySQL related erros occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo) {
+		//check that tag exists before deleting it
+		if($this->tagImageId === null || $this->tagHashtagId === null) {
+			throw(new \PDOException("unable to delete a hashtag that does not exist"));
+		}
+		//create a query template
+		$query = "DELETE FROM Tag WHERE tagImageId = :tagImageId AND tagHashtagId = :tagHashtagId";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the place holder in the template
+		$parameters = ["tagImageId" => $this->tagImageId, "tagHashtagId" => $this->tagHashtagId];
+		$statement->execute($parameters);
+	}
+
+	/** gets the tag by tagImageId
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $tagImageId tagHashtag id to search for
+	 * @return tag|null tag found or null if not found
+	 * @throws \PDOException when mySQL related erros occur
+	 * @throws |TypeError when variables are not the correct data type
+	 **/
+	public static function getTagByTagImageId(\PDO $pdo, int $tagImageId) {
+		//sanitize the tagHashtagId before searching
+		if($tagImageId <= 0) {
+			throw(new \PDOException("tagHashtag id is not positive"));
+		}
+		// create query templatte
+		$query = "SELECT tagImageId, tagHashtagId FROM tag WHERE tagImageId = :tagImageId";
+		$statement = $pdo->prepare($query);
+
+		//bind the tagHashtag Id to theplace holder in teh template
+		$parameters = ["tagImageId => $tagImageId"];
+		$statement->execute($parameters);
+
+		//build an arary of tags
+		$tags = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement ->fetch()) !==false) {
+			try {
+				$tag = new tag($row["tagImageId"], $row["tagHashtagId"]);
+				$tags[$tags->key()] = $tag;
+				$tags->next();
+			} catch(\Exception $exception) {
+				//if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($tags);
+	}
+
+	/**
+	 * gets the tag by tagHashtagId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $tagHashtagId tagHashtag id to search for
+	 * @return tag|null tag found or null if not found
+	 * @throws \PDOException when mySQL related erros occur
+	 * @throws |TypeError when variables are not the correct data type
+	 **/
+	public static function getTagBytagHashtagId(\PDO $pdo, int $tagHashtagId) {
+		//sanitize the tagHashtagId before searching
+		if($tagHashtagId <= 0) {
+			throw(new \PDOException("Tag Hashtag Id is not positive"));
+		}
+		// create query templatte
+		$query = "SELECT tagImageId, tagHashtagId FROM tag WHERE tagHashtagId = :tagHashtagId";
+		$statement = $pdo->prepare($query);
+
+		//bind the tagHashtag Id to theplace holder in teh template
+		$parameters = ["tagHashtagId" => $tagHashtagId];
+		$statement->execute($parameters);
+
+		//build an array of hashtag tags
+		$tags = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$tag = new tag($row["tagHashtagId"], $row["tagHashtagId"]);
+				$tags[$tags->key()] = $tag;
+				$tags->next();
+			} catch(\Exception $exception) {
+				//if the row could not be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($tags);
+	}
+
+	/**
+	 * updates this tag in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related erros occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	/*		public function update(\PDO $pdo) {
+				//enforce the tagHashtagId is not null
+				if($this->tagHashtagId === null) {
+					throw(new \PDOException("unable to update a tag that does not exist"));
+				}
+
+				//create query template
+				$query = "UPDATE tag SET tagHashtagId = :tagHashtagId, tagImageId = :tagImageId";
+				$statement = $pdo->prepare($query);
+
+				//bind the member variables to the place holders in the template
+				$parameter = ["tagHashtagId" => $this->tagHashtagId, "tagImageId" => $this->tagImageId];
+				$statement->execute($parameter);
+			}*/
+
+	/**
+	 * gets tag by image id and hashtag id
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $tagImageId image id to search for
+	 * @param int $tagHashtagId hashtag id to search for
+	 * @return tag|null tag if found, null if not
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getTagByImageIdAndHashtagId(\PDO $pdo, int $tagImageId, int $tagHashtagId) {
+		//sanitize the image id and the hashtag id before searching
+		if($tagImageId < 0) {
+			throw (new \PDOException("beer id is not positive"));
+		}
+		if($tagHashtagId < 0) {
+			throw (new \PDOException("tag id is not positive"));
+		}
+		//create a query template
+		$query = "SELECT tagImageId, tagHashtagId FROM tag WHERE tagImageId = :tagImageId AND tagHashtagId = :tagHashtagId;";
+		$statement = $pdo->prepare($query);
+		//bind the variables to the placeholders in the template
+		$parameters = ["tagImageId" => $tagImageId, "tagHashtagId" => $tagHashtagId];
+		$statement->execute($parameters);
+		//grab the beer tag from mySQL
+		try {
+			$tag = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$tag = new tag($row["tagHashtagId"], $row["tagHashtagId"]);
+			}
+		} catch(\Exception $exception) {
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($tag);
+	}
+
+	/**
+	 * formats state variables for JSON serialization
+	 *
+	 * @return array resulting state variables to serialize
+	 **/
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+		return ($fields);
+	}
+}
---
+++ test/ImageTagTest.php
@@ -0,0 +1,226 @@
+<?php
+namespace Edu\Cnm\Flek\Test;
+
+use Edu\Cnm\Flek\{
+	Hashtag, Tag, Image, Profile
+};
+
+//grab the project test parameters
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
+require_once("FlekTest.php");
+
+/**
+ * full php test of the class Tag
+ *
+ * @see \Edu\Cnm\Flek\Tag
+ **/
+class TagTest extends FlekTest {
+
+	//User that will tag the images
+	protected $PROFILE = null;
+
+	/**
+	 * image that contains the tag
+	 **/
+	protected $IMAGE = null;
+	/**
+	 * tag that is linked to image
+	 * @var Tag tag
+	 **/
+	protected $TAG = null;
+	/**
+	 * hashtag that will include in the tag
+	 **/
+	protected $HASHTAG = null;
+
+	/**
+	 * create dependent objects for each foreign key before running test
+	 **/
+	public final function setUp() {
+		// run the default setUp method first
+		parent::setUp();
+
+		// create and insert a Hashtag to own the test Tag
+		$this->hashtag = new Hashtag(null, "booya", "content");
+		$this->hashtag->insert($this->getPDO());
+
+		//access and activation token & salt and hash generation
+		$this->VALID_PROFILEACCESSTOKEN = bin2hex(random_bytes(16));
+		$this->VALID_PROFILEACTIVATIONTOKEN = bin2hex(random_bytes(16));
+		$this->salt = bin2hex(random_bytes(32));
+		$this->hash = hash_pbkdf2("sha256", "abc123", $this->salt, 262144);
+
+		// create and insert an Image to own the test Tag
+		$this->new Profile(null, $this->);
+		$this->profile->insert($this->getPDO());
+		//create and insert a Hashtag to own the test tag
+		$this->new Hashtag(null, $this->);
+
+		//create a tag to be linked with Image
+		$this->tag = new Tag(null, "");
+		$this->tag->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a valid Tag and verifying that mySQL data matches
+	 **/
+	public function testInsertValidTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tag::getTagByTagId($this->getPDO(), $tag->getTagImageId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertCounts(1, $results);
+		//grab the results from the array and validate them
+		$pdoTag = $results[0];
+		$this->assertEquals($pdoTag->getTagImageId(), $this->image->getImageId());
+		$this->assertEquals($pdoTag->getTagHashtagId(), $this->hashtag->getHashtagId());
+	}
+
+	/**
+	 * test inserting a Tag that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+
+	public function testInsertInvalidTag() {
+		// create a Tag with a non null tag id and watch it fail
+		$tag = new Tag(FlekTest::INVALID_KEY, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+	}
+
+	/**
+	 * test creating a Tag then deleting it
+	 **/
+	public function testDeleteValidTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// delete the Tag from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$tag->delete($this->getPDO());
+		// grab the data from mySQL and enforce the Tag does not exist
+		$pdoTag = Tag::getTagByTagIdAndHashTagId($this->getPDO(), $tag->getTagImageId(), $tag->getTagHashtagId());
+		$this->assertNull($pdoTag);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("tag"));
+	}
+
+	/**
+	 * test deleting a Tag that does not exist
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testDeleteInvalidTag() {
+		// create a Tag and try to delete it without actually inserting it
+		$tag = new Tag(null, $this->image->tagImageId, $this->hashtag->tagHashtagId);
+		$tag->delete($this->getPDO());
+
+		/**
+		 * testing a tag by valid image id
+		 **/
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		//create a new tag and insert it into mySQL
+		$tag = new Tag($this->image->getImageId(), $this->hashtag->getHashtagId());
+		$tag->insert($this->getPDO());
+		//grab the data from mySQL and enforce that the fields match our expectations
+		$results = Tag::getImageByImageId($this->getPDO, tag->getImageId);
+	$this->assertCount(1, $results);
+	$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Flek\\Tag", $results);
+	//grab the results from the array and validate them
+	$pdoTag = $results[0];
+	$this->assertEquals($pdoTag->getTagImageId(), $this->image->getImageId());
+	$this->assertEquals($pdoTag->getHashtagId(), $this->hashtag->getHashtagId());
+}
+
+	/**
+	 * testing get tag by valid
+	 **/
+	/**
+	 * test inserting a Tag editing it and then updating it
+	 **/
+	public function testUpdateValidTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// edit the Tag and update it in mySQL
+		$tag->setTagName($this->tagImageId, $this->tagHashtagId2);
+		$tag->update($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoTag = Tag::getTagByTagId($this->getPDO(), $tag->getTagId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertEquals($pdoTag->getTagName(), $this->tagImageId, $this->tagHashtagId2);
+	}
+	/**
+	 * test updating a Tag that does not exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testUpdateInvalidTag() {
+		// create a Tag with a non null tag id and watch it fail
+		$tag = new Tag(null,  $this->tagImageId, $this->tagHashtagId);
+		$tag->update($this->getPDO());
+	}
+
+	/**
+	 * test grabbing a Tag by tag content
+	 **/
+	public function testGetValidTagByTagContent() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tag::getTagByTagContent($this->getPDO(), $tag->getTagName());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertCount(1, $results);
+		// grab the result from the array and validate it
+		$pdoTag = $results[0];
+		$this->assertEquals($pdoTag->getTagName(), $this->tagImageId, $this->tagHashtagId);
+	}
+	/**
+	 * test grabbing a Tag that does not exist
+	 **/
+	public function testGetInvalidTagByTagId() {
+
+		$tag = Tag::getTagByTagId($this->getPDO(), FlekTest::INVALID_KEY);
+		$this->assertNull($tag);
+	}
+	/**
+	 * test grabbing a Tag by content that does not exist- not in documentation
+	 **/
+	public function testGetInvalidTagByTagContent() {
+		// grab a tag by content that does not exist
+		$tag = Tag::getTagByTagContent($this->getPDO(), "this is not a valid Tag");
+		$this->assertCount(0, $tag);
+	}
+	/**
+	 * test grabbing all Tags
+	 **/
+	public function testGetAllValidTags() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("tag");
+		// create a new Tag and insert to into mySQL
+		$tag = new Tag(null, $this->tagImageId, $this->tagHashtagId);
+		$tag->insert($this->getPDO());
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Tag::getAllTags($this->getPDO());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tag"));
+		$this->assertCount(1, $results);
+		// grab the result from the array and validate it
+		$pdoTag = $results[0];
+		$this->assertEquals($pdoTag->getTagName(), $this->tagImageId, $this->tagHashtagId);
+	}
+
+}
+
+
+
