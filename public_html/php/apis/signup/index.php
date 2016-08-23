f8e6f7f7b9bd5d2147a6b3cc272aa2050d680f0c
Christina Sosa <csosa2106@gmail.com>
2016-08-23 16:54:43 -0600

    created try block to connect to mySQL https://app.asana.com/0/161549255700615/170520114054324

--- public_html/php/apis/signup/index.php
+++ public_html/php/apis/signup/index.php
@@ -1,1 +1,32 @@
 <?php
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Flek;
+
+/**
+ * api for signup
+ *
+ * @author Christina Sosa <csosa4@cnm.edu>; referenced Derek Mauldin <derek.e.mauldin@gmail.com>
+**/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try{
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/flek.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER
+	["REQUEST_METHOD"];
+}
