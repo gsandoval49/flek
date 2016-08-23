eb2c431f3aa8f7deafa68f84a4806b93caf14065
Robert Harding <robisonfire@gmail.com>
2016-08-23 16:36:21 -0600

    filling out initial method https://app.asana.com/0/161555263951762/170520114054336

--- public_html/php/apis/image/index.php
+++ public_html/php/apis/image/index.php
@@ -38,7 +38,14 @@ $reply->data = null;
 try {
 	//grab the mySQL connection
 
-	$pdo = connectToEncryptedMySQL("/etc/apache2/flek-mysql/image.ini");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/flek.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] :$_SERVER["REQUEST_METHOD"];
+	$reply->method = $method;
+
+	//sanitize the input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
 
 
 }
