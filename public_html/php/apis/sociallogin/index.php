b6859a83f714a221a807bbbb5aad94a3465ae7b2
Robert Harding <robisonfire@gmail.com>
2016-08-23 15:42:36 -0600

    setting up Image api https://app.asana.com/0/161555263951762/170520114054336

--- public_html/php/apis/sociallogin/index.php
+++ public_html/php/apis/sociallogin/index.php
@@ -5,3 +5,18 @@
  * Date: 8/22/2016
  * Time: 9:14 PM
  */
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once("/etc/apache2/flek-mysql/encrypted-config.php");
+
+use Edu\Cnm\Flek\(Image);
+
+
+/**
+ * api for Image class
+ *
+ * @author Rob Harding
+ **/
+
+
+
