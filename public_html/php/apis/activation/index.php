cba67c21c65436260a948736aab555c196d0375f
Christina Sosa <csosa2106@gmail.com>
2016-08-23 15:49:09 -0600

    set up the file and created the GET request https://app.asana.com/0/161549255700615/170520114054330

--- public_html/php/apis/activation/index.php
+++ public_html/php/apis/activation/index.php
@@ -1,7 +1,39 @@
 <?php
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Flek\Activation;
+
 /**
- * Created by PhpStorm.
- * User: Sosa
- * Date: 8/23/16
- * Time: 9:46 AM
- */
+ * api for Activation
+ *
+ * @author Christina Sosa <csosa4@cnm.edu>; referenced Derek Mauldin <derek.e.mauldin@gmail.com>
+**/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo =  connectToEncryptedMySQL("/etc/apache2/capstone-mysql/flek.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//handle GET request - if id is present, that activation is returned, otherwise all activations are returned
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+	//get the Sign up based on the given field
+
+	}
+}
