d25384a2e806aa6a7bcbd61bb97e5f0c1b40c1f5
Robert Harding <robisonfire@gmail.com>
2016-08-23 16:11:01 -0600

    added verify the session and prepare an empty reply https://app.asana.com/0/161555263951762/170520114054336

--- public_html/php/apis/image/index.php
+++ public_html/php/apis/image/index.php
@@ -25,3 +25,17 @@ use Edu\Cnm\Flek\(Image);
  * @author Rob Harding
  **/
 
+
+// verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+
+
+
+
