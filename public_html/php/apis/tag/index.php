6f58f534bfd7a27ce54ba9304788e56c591982e8
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-24 15:36:19 -0600

    added another directory. will work on this later. still can't get rid of yellow.

--- public_html/php/apis/tag/index.php
+++ public_html/php/apis/tag/index.php
@@ -1,11 +1,11 @@
 <?php
 
 /*require once here - double check if dirname(_DIR_) is needed*/
-require_once dirname(__DIR__, 2) . "classes/autoload.php";
-require_once "/lib/xsrf.php";
+require_once dirname(__DIR__, 2) . "autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
 require_once ("/etc/apache2/flek-mysql/encrypted-config.php");
 
-use Edu\Cnm\Flek;
+use Edu\Cnm\Flek\Tag;
 
 /**
  * api for the tag class
@@ -18,5 +18,8 @@ if(session_start() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
-
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
 
