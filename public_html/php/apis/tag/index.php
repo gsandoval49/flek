1d288be86670bc23d2bcc055ce05fbd1a9a0897c
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-24 16:32:53 -0600

    handle get request code and json encode and decode lines added.

--- public_html/php/apis/tag/index.php
+++ public_html/php/apis/tag/index.php
@@ -23,3 +23,69 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 
+// huge try block and grabbing mySQL connection to .ini files
+try {
+    $pdo = connectToEncryptedMySQL("/etc/apache2/flek-mysql/tag.ini");
+    $method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+    // sanitize input
+    $id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+    // ensuring the id is valid for methods that require it, no \ wack needed in InvalidArgumentException
+    if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+        throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+    }
+
+    // handle GET request - if id is present, that Tag is returned
+    if(method === "GET") {
+        // set XSRF cookie
+        setXsrfCookie();
+
+        // get a specific tag or all tags and update reply
+        if(empty($id) === false) {
+            $tag = Tag::getTagByTagId($pdo, $id);
+            if($tag !== null) {
+                $reply->data = $tag;
+            }
+        } else {
+            $tags = Tag::getsAllTags($pdo);
+            if($tags !== null) {
+                $reply->data = $tags;
+            }
+        }
+    } else if($method === "PUT" || $method === "POST") {
+        // give us Json string doesn't give us object yet, only json encode
+        verifyXsrf();
+        // "unpacking angular's Json
+        $requestContent = file_get_contents("php://input");
+        // get string, decode, and make into object
+        $requestObject = json_decode($requestContent);
+
+        // make sure the Tag name is available
+        if(empty($requestObject->tagName) === true) {
+            throw(new \InvalidArgumentException("No tag name.", 405));
+        }
+
+        // perform the actual put or post
+        if($method = "PUT")
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
