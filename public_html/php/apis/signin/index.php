02186ab35e7c4a8d13a12811258670a09ddf635e
Christina Sosa <csosa2106@gmail.com>
2016-08-24 16:01:17 -0600

    inserted require once and start session https://app.asana.com/0/161549255700615/170520114054326

--- public_html/php/apis/signin/index.php
+++ public_html/php/apis/signin/index.php
@@ -1,7 +1,16 @@
 <?php
+require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Flek\Profile;
+
 /**
- * Created by PhpStorm.
- * User: Sosa
- * Date: 8/24/16
- * Time: 3:47 PM
- */
+ * api for signin
+ *
+ * @author Christina Sosa <csosa4@cnm.edu>
+**/
+//start session
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
