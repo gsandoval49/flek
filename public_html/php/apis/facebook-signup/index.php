59ee2c5800189e936547d865fcf759141a3584aa
Christina Sosa <csosa2106@gmail.com>
2016-08-25 13:41:44 -0600

    created require once https://app.asana.com/0/161549255700615/170520114054365

--- public_html/php/apis/facebook-signup/index.php
+++ public_html/php/apis/facebook-signup/index.php
@@ -1,4 +1,20 @@
 <?php
+require_once dirname(dirname(__DIR__)) . "/classes/autoload.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Flek\Profile;
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 //use this in the body tag of each page we want it
 <script>
