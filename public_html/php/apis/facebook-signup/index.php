3ffb5d27b9183b411a571820bfc40a66ad488ed5
Christina Sosa <csosa2106@gmail.com>
2016-08-25 13:39:43 -0600

    added code to add to body tag for facebook login in a comments section (to be used somewhere else) https://app.asana.com/0/161549255700615/170520114054365

--- public_html/php/apis/facebook-signup/index.php
+++ public_html/php/apis/facebook-signup/index.php
@@ -1,1 +1,27 @@
 <?php
+/*
+//use this in the body tag of each page we want it
+<script>
+window.fbAsyncInit = function() {
+	FB.init({
+      appId      : '533126920205402',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+(function(d, s, id){
+	var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+//like button
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>
+</script>*/
