ab7b289eacc41982be7071e6305112974e370aa7
Christina Sosa <csosa2106@gmail.com>
2016-08-25 13:45:00 -0600

    inserted start session https://app.asana.com/0/161549255700615/170520114054365

--- public_html/php/apis/facebook-signup/index.php
+++ public_html/php/apis/facebook-signup/index.php
@@ -4,6 +4,16 @@ require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Flek\Profile;
 
+/**
+ * api for facebook signup
+ *
+ * @author Christina Sosa <csosa4@cnm.edu>
+ **/
+//start session
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
 
 
 
