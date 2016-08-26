b66bca5af630e34c45e5c95c846ba55539e20de0
Christina Sosa <csosa2106@gmail.com>
2016-08-26 10:13:34 -0600

    created require once and use https://app.asana.com/0/161549255700615/170520114054328

--- public_html/php/apis/signout/index.php
+++ public_html/php/apis/signout/index.php
@@ -1,1 +1,13 @@
 <?php
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\Flek\Profile;
+
+/**
+ * api for Signout
+ *
+ * @author Christina Sosa <csosa4@cnm.edu>
+ **/
