e604c238582e853d5a9ff4613ff1db0afe0d1de6
Christina Sosa <csosa2106@gmail.com>
2016-08-26 10:28:37 -0600

    added try block https://app.asana.com/0/161549255700615/170520114054328

--- public_html/php/apis/signout/index.php
+++ public_html/php/apis/signout/index.php
@@ -19,5 +19,13 @@ try {
 	//grab the mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/flek.ini");
 	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+	if($method === "GET") {
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			session_start();
+		}
+		$_SESSION = [];
+		$reply->message = "You are now signed out.";
+	}
 
 }
