d2c876d9b2c00141cd34a37378f370fd16d730c7
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-28 20:49:41 -0600

    adding partial files with head utils code.

--- public_html/php/apis/mail/index.php
+++ public_html/php/apis/mail/index.php
@@ -30,19 +30,8 @@ function mailGunner ( $domain, $senderName, $senderMail, $receiverName, $receive
             "text" => $message
         ]
     );
-    /*
-        // inform the user of the result
-        if($result->http_response_code !== 200) {
-            throw(new RuntimeException("unable to send email", $result->http_response_code));
-        }
-        $reply->message = "Thank you for reaching out. I'll be in contact shortly!";
-        }  else {
-            throw(new InvalidArgumentException("Invalid HTTP method request", 405));
-        }
 
-    */
-
-    # Iterate through the results and echo the message IDs.
+    // # Iterate through the results and echo the message IDs.
     $logItems = $result->http_response_body->items;
     foreach($logItems as $logItem){
         $mailMessageId = $logItem->message_id;
---
+++ public_html/php/partials/head-utils.php
@@ -0,0 +1,36 @@
+<DOCTYPE html>
+<html lang="" ng-app="FlekApp">
+<head>
+    <!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+    <!-- set base for relative links - to enable pretty URLs -->
+    <base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/";?>">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
+
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <!--Angular JS Libraries incorporated from templating angular class-->
+    <?php $ANGULAR_VERSION = "1.5.8";?> <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-messages.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-route.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-animate.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/1.3.3/ui-bootstrap-tpls.min.js"></script>
+    <!-- load OUR angular files. -->
+    <script src="angular/meow-app.js"></script>
+    <script src="angular/route-config.js"></script>
+    <script src="angular/directives/bootstrap-breakpoint.js"></script>
+
+</head>
+</html>
