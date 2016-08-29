5d5a40821564e04c6badc3fca9e4f14a893d259f
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-28 23:28:17 -0600

    added home section with slogan and button.

---
+++ public_html/angular/pages/home.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-xs-12">
+        <div class="jumbotron text-right">
+            <h1><i class="pick-icon"></i> Flek</h1>
+            <p class="lead">Connect artists to community. Bring skills & crafts to your home & business. Beautify your space.</p>
+            <a href="sign-up" class="btn btn-warning btn-lg">Create Account</a>
+        </div>
+    </div>
+</div>
