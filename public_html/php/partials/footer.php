3f155d8f97a69119dd5da66e352ec04173d09e40
Giles Sandoval <gsandoval2277@gmail.com>
2016-08-28 22:52:56 -0600

    code for header and footer as templating for the site.

---
+++ public_html/php/partials/footer.php
@@ -0,0 +1,10 @@
+<!--begin footer -->
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12">
+                &copy;&nbsp;2016 Flek
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ public_html/php/partials/header.php
@@ -0,0 +1,29 @@
+<header ng-controller="navController">
+    <!-- bootstrap breakpoint directive to control collapse behavior -->
+    <bootstrap-breakpoint></bootstrap-breakpoint>
+
+    <nav class="navbar navbar-default navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" aria-expanded="false" ng-click="navCollapsed = !navCollapsed">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index">Flek</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" uib-collapse="navCollapsed">
+
+                <ul class="nav navbar-nav navbar-right" >
+                    <li><a href="index">Home</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="sign-up">Sign Up</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
