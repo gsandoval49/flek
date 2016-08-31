b6bbcdb9ffe382149c24b5c19be48c51c1052b26
Christina Sosa <csosa2106@gmail.com>
2016-08-30 21:30:15 -0600

    added and took out comments https://app.asana.com/0/161555263951762/170520114054328

--- public_html/php/apis/signout/index.php
+++ public_html/php/apis/signout/index.php
@@ -9,18 +9,28 @@ use Edu\Cnm\Flek\Profile;
 /**
  * api for Signout
  *
- * @author Christina Sosa <csosa4@cnm.edu>
+ * @author Christina Sosa <csosa4@cnm.edu> with borrowed code from BrewCrew
  **/
+
+//verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+
 //prepare default error message
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
 try {
 	//grab the mySQL connection
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/flek.ini");
+
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
 	if($method === "GET") {
+
 		if(session_status() !== PHP_SESSION_ACTIVE) {
 			session_start();
 		}
