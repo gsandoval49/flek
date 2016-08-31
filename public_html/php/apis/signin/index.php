dd287d323d8ffa9b0d691bfe29dd546c99f420e3
Christina Sosa <csosa2106@gmail.com>
2016-08-31 16:40:04 -0600

    fixed require once errors https://app.asana.com/0/161555263951762/170520114054324

--- public_html/php/apis/signin/index.php
+++ public_html/php/apis/signin/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once dirname(dirname(__DIR__, 2)) . "/classes/autoload.php";
-require_once dirname(dirname(__DIR__, 2)) . "/lib/xsrf.php";
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Flek\Profile;
 
