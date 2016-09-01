570437951f1f65b867b9f65ccfbe1cdc7cca8e5a
Christina Sosa <csosa2106@gmail.com>
2016-08-31 18:33:55 -0600

    fixed errors https://app.asana.com/0/161555263951762/170520114054326

--- public_html/php/apis/facebook-signup/index.php
+++ public_html/php/apis/facebook-signup/index.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . "/vendor/autoload.php";
 /*require_once __DIR__ . "/facebook-sdk-v5/autoload.php";*/
-require_once (dirname(dirname(__DIR__)) . "/classes/autoload.php");
-require_once (dirname(dirname(__DIR__)) . "/lib/xsrf.php");
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Flek;
 
--- public_html/php/apis/signin/index.php
+++ public_html/php/apis/signin/index.php
@@ -9,15 +9,17 @@ use Edu\Cnm\Flek\Profile;
  *n
  * @author Christina Sosa <csosa4@cnm.edu>
 **/
-//start session
-if(session_status() !== PHP_SESSION_ACTIVE) {
-	session_start();
-}
+
 //prepare an empty reply
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 try {
+
+	//start session
+	if(session_status() !== PHP_SESSION_ACTIVE) {
+		session_start();
+	}
 
 	//grab mySQL statement
 	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/flek.ini");
@@ -25,10 +27,14 @@ try {
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
+	if($method === "GET"){
+		//set xsrf token
+		verifyXsrf();
+	}
 	//perform the post
-	if($method === "POST") {
+	else if($method === "POST") {
 
-		//verifyXsrf();
+		verifyXsrf();
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
@@ -36,23 +42,23 @@ try {
 		if(empty($requestObject->profileEmail) === true) {
 			throw(new \InvalidArgumentException("Invalid email address."));
 		} else {
-			$email = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
+			$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
 		}
-		if(empty($requestObject->profilePassword) === true) {
+		if(empty($requestObject->password) === true) {
 			throw(new \InvalidArgumentException("Must enter a password."));
 		} else {
-			$password = filter_var($requestObject->profilePassword, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+			$password = filter_var($requestObject->password, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		}
 
 		//create a profile
 		$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
 
-		if(empty($profile)) {
+		if(empty($profile) === true) {
 			throw(new \InvalidArgumentException("Invalid Email"));
 		}
 
 		//hash for $password
-		$hash = hash_pbkdf2("sha512", "abc123", $this->$salt, 262144);
+		$hash = hash_pbkdf2("sha512", $password, $profile->$getProfileSalt, 262144);
 
 		//verify hash is correct
 		if($hash !== $profile->getProfileHash()) {
