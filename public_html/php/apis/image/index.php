da1a5967e0aa31a1c2de7ec3e14eaa6d3afb5915
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-01 10:37:54 -0600

    think we need this array but could be false https://app.asana.com/0/161555263951762/170520114054336

--- public_html/php/apis/image/index.php
+++ public_html/php/apis/image/index.php
@@ -82,17 +82,19 @@ try {
 			if($image !== null) {
 				$reply->data = $image;
 			}
-		} elseif(empty($id) === false) {
+		} elseif(empty($profile) === false) {
 			$image = Image::getImageByImageProfileId($pdo, $imageProfileId);
 			if($image !== null) {
 				$reply->data = $image;
 			}
-		} elseif(empty($id) === false) {
+		} elseif(empty($description) === false) {
 			$image = Image::getImageByImageDescription($pdo, $imageDescription);
 			if($image !== null) {
 				$reply->data = $image;
 			}
-		} else {
+		}
+		//DO we need to include secure url and public id
+			else {
 			$images = Image::getAllImages($pdo);
 			if($images !== null) {
 				$reply->data = $images;
@@ -123,6 +125,24 @@ try {
 					$image->insert($pdo);
 				}
 				$reply->message = "Image created";
+			}
+				// files array to send images
+			public static function upload($file, $options = \SplFixedArray::array() {
+				Array
+					{
+					[public_id] => c87hg9xfxrd4itiim3t0
+					[version] => 1371995958
+  					[signature] => f8645b000be7d717599affc89a068157e4748276
+					[width] => 864
+  					[height] => 576
+  					[format] => jpg
+					[resource_type] => image
+					[created_at] => 2013-06-23T13:59:18Z
+					[bytes] => 120253
+  					[type] => upload
+					[url] => http://res.cloudinary.com/demo/image/upload/v1371995958/c87hg9xfxrd4itiim3t0.jpg
+					[secure_url] => https://res.cloudinary.com/demo/image/upload/v1371995958/c87hg9xfxrd4itiim3t0.jpg
+
 			}
 
 		} elseif($method === "DELETE") {
