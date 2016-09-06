f65a8e895728fffd85d6c0e062b5a82e37ccc00f
Giles Sandoval <gsandoval2277@gmail.com>
2016-09-06 08:56:46 -0600

    created lines of code for throwing exception for invalid email

--- public_html/php/apis/mail/index.php
+++ public_html/php/apis/mail/index.php
@@ -3,6 +3,7 @@
 require_once dirname(__DIR__, 2) . "/classes/autoload.php";
 require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 4) . "/vendor/autoload.php";
 
 use Edu\Cnm\Flek\Mail;
 
@@ -21,6 +22,9 @@ $reply->status = 200;
 $reply->data = null;
 
 try {
+
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/flek.ini");
+
 	//mailgun configuration that Dylan sent me. We're getting mailgun config
 	$config = readConfig("/etc/apache2/capstone-mysql/flek.ini");
 	$mailgun = json_decode($config["mailgun"]);
@@ -28,6 +32,11 @@ try {
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
+	// must be logged in to get messages
+	if(empty($_SESSION["profile"]) === true) {
+		throw(new \InvalidArgumentException("you must be logged in"));
+	}
+	// need to call on profiles and verify
 	// handle GET request - just kick them out
 	if($method === "GET") {
 		//set XSRF cookie
@@ -44,7 +53,9 @@ try {
 			$reply->data = Mail::getMailByMailSenderId($pdo, $mailSenderId)->toArray();
 		} else {
 			$reply->data; // do wel call data === "mails"
-		}
+		} /*else {
+			throw(new \InvalidArgumentException ("Invalid login to access your messages"));
+		}*/
 	} // moved closed bracket here as part of GET code
 
 		// TODO
@@ -52,19 +63,41 @@ try {
 		// grab 2 profiles angular will get for sender & receiver
 
 
+
 	/* SHOULD THIS BE AN "ELSE IF" OR "IF" */
-	if($method === "POST") {
+	else if($method === "POST") {
 		verifyXsrf();
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
 
-
 		// throw out blank fields
+		if(empty($requestObject->senderName) === true) {
+			throw(new \InvalidArgumentException("your name is required", 400));
+		}
+		if(empty($requestObject->senderEmail) === true) {
+			throw(new \InvalidArgumentException("email is required", 400));
+		}
+		if(empty($requestObject->receiverName) === true) {
+			throw(new \InvalidArgumentException("receiver name is required", 400));
+		}
+		if(empty($requestObject->receiverEmail) === true) {
+			throw(new \InvalidArgumentException("receiver email is required", 400));
+		}
 		if(empty($requestObject->subject) === true) {
 			throw(new \InvalidArgumentException("subject is required", 400));
 		}
 		if(empty($requestObject->message) === true) {
 			throw(new \InvalidArgumentException("message is required", 400));
+		}
+
+		// for both sender AND receiver:
+		// grab the profile by Email
+		// if either are null, throw an exception
+		if(Profile::getProfileByProfileEmail($pdo, $requestObject->senderEmail) === null) {
+			throw(new \InvalidArgumentException("email is invalid"));
+		}
+		if(Profile::getProfileByProfileEmail($pdo, $requestObject->receiverEmail) === null) {
+			throw(new \InvalidArgumentException("email is invalid"));
 		}
 
 		// start the mailgun client
@@ -74,20 +107,20 @@ try {
 		// send the message
 		$result = $mailGunslinger->sendMessage($mailgun->domain, [
 				// TODO change after profiles
-				"from" => "$name <$email>",
-				"to" => "foo@example.com",
+				"from" => $requestObject ->senderName . "<" .  $requestObject->senderEmail . ">",
+				"to" => $requestObject ->receiverName . "<" .  $requestObject->receiverEmail . ">",
 				"subject" => $requestObject->subject,
 				"text" => $requestObject->message
 			]
 		);
 
 		// inform the user of the result
-		if($result->http_response_code !== 200) {
+		/*if($result->http_response_code !== 200) {
 			throw(new RuntimeException("unable to send email", $result->http_response_code));
 		}
 		$reply->message = "Your message has been sent.";
 	} else {
-		throw(new InvalidArgumentException("Invalid HTTP method request", 405));
+		throw(new \InvalidArgumentException("Invalid HTTP method request", 405));*/
 	}
 
 	// update reply with exception information
