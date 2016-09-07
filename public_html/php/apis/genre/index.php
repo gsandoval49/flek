4471008ff5b10d1cf58b88274e5f1aa2074f140b
Robert Harding <robisonfire@gmail.com>
2016-09-07 14:59:42 -0600

    added comments and fixed "use" to incorporate image https://app.asana.com/0/161555263951762/170520114054338

--- public_html/php/apis/genre/index.php
+++ public_html/php/apis/genre/index.php
@@ -7,8 +7,8 @@ require_once dirname(__DIR__, 2) . "/classes/autoload.php";
 require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-
-use Edu\Cnm\Flek\Genre;
+/*Genre will have to incorporate Image*/
+use Edu\Cnm\Flek\{Genre, Image};
 
 
 /**
