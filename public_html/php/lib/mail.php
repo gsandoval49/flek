cfb4efe3f2c382b2487d111f5991655ed103d29f
Christina Sosa <csosa2106@gmail.com>
2016-09-08 15:06:47 -0600

    created a mail.php in lib to call mailgun function in signup https://app.asana.com/0/161555263951762/170520114054324

---
+++ public_html/php/lib/mail.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * function for the Mail class
+ *
+ * @author Christina Sosa <csosa4@cnm.edu>; Referenced Gerald Sandoval <gsandoval16@cnm>
+ **/
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 4) . "/vendor/autoload.php";
+
+
+function mailGunslinger ($senderName, $senderEmail, $receiverName, $receiverEmail, $subject, $message) {
+	$config = readConfig("/etc/apache2/capstone-mysql/flek.ini");
+	$mailgun = json_decode($config["mailgun"]);
+// now $mailgun->domain and $mailgun->apiKey exist
+	// start the mailgun client
+	$client = new \Http\Adapter\Guzzle6\Client();
+	$mailGunner = new \Mailgun\Mailgun($mailgun->apiKey, $client);
+	// send the message
+	$result = $mailGunslinger->sendMessage($mailgun->domain, [
+			"from" => "$senderName <$senderEmail>",
+			"to" => "$receiverName <$receiverEmail>",
+			"subject" => $subject,
+			"html" => $message,
+			"text" => html_entity_decode($message)
+		]
+	);
+	if($result->http_response_code !== 200) {
+		throw(new RuntimeException("unable to send email", $result->http_response_code));
+	}
+	//split the result before the at symbol
+	$atIndex = strpos($result->http_response_body->id, "@");
+	if($atIndex === false) {
+		throw (new RangeException("unable to send email", 503));
+	}
+	$mailgunMessageId = substr($result->http_response_body->id, 1, $atIndex - 1);
+	return $mailgunMessageId;
+}
