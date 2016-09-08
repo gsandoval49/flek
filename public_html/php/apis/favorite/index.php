a8b65480bd532363cee8dc3c503c0487fc3b2c3e
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-08 14:00:37 -0600

    fixed some braces https://app.asana.com/0/161552024814128/170520114054344

--- public_html/php/apis/favorite/index.php
+++ public_html/php/apis/favorite/index.php
@@ -4,7 +4,7 @@ require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\Flek\ {
+use Edu\Cnm\Flek\{
 	Favorite, Profile
 };
 
@@ -50,84 +50,87 @@ try {
 	if($method === "GET") {
 		// Set XSRF cookie
 		setXsrfCookie("/");
-	}
-	//get favorite or all favorites and update reply
-	if(empty($favoriteeId) === false && empty($favoriterId) === false) {
-		$favorite = Favorite::getFavoriteByFavoriteeIdAndFavoriterId($pdo, $favoriteeId, $favoriterId);
-		if($favorite !== null) {
-			$reply->data = $favorite;
-		}
-// Get a favorites from FavoriterId where favorites is the favoriteeId and update reply
-		if(empty($favoriteeid) === false) {
+
+		//get favorite or all favorites and update reply
+		if(empty($favoriteeId) === false && empty($favoriterId) === false) {
+			$favorite = Favorite::getFavoriteByFavoriteeIdAndFavoriterId($pdo, $favoriteeId, $favoriterId);
+			if($favorite !== null) {
+				$reply->data = $favorite;
+			}
+		} // Get a favorites from FavoriterId where favorites is the favoriteeId and update reply
+		elseif(empty($favoriteeid) === false) {
 			$favorite = Edu\Cnm\Flek\Favorite::getFavoriteByFavoriterId($pdo, $favoriteeid);
 			if($favorite !== null) {
 				$reply->data = $favorite;
 			}
-			//else{
-			//$favorites = Edu\Cnm\Flek\Favorite::getFavoriteByFavoriteeIdAndFavoriterId($pdo, $favoriterId, $favoriteeId);
-			//if($favoriterId !== null) {
-			//$reply->data = $favorite;
-			//}
-
-			//-------------------------POST------------------------------
-		} elseif($method === "POST") {
-			// Set XSRF cookie
-			verifyXsrf();
-			$requestContent = file_get_contents("php://input");
-			$requestObject = json_decode($requestContent);
-			//make sure favorite profile is available
-			//request object...whatever is on the angular form....
-
-			// make sure profileId are available
-			if(empty($requestObject->favoriteeId) === true) {
-				throw(new InvalidArgumentException("no favorite available", 405));
-			}
-			//if(empty($requestObject->id) === true) {
-				//throw(new InvalidArgumentException("no profile available", 405));
-			//}
-			//created new profile and insert into database
-			//$profile = new Profile(null, $requestObject->profileEmail, $requestObject->profileLocation, $requestObject->profileBio, $hash, $salt, $profileAccessToken, $profileActivationToken);
-			//$profile->insert($pdo);
-
-			// create new favorite and insert into the database
-			$favorite = new Edu\Cnm\Flek\Favorite(null, $requestObject->getProfileId, $requestObject->getId);
-			$favorite->insert($pdo);
-			// update reply
-			$reply->message = "Favorite created OK";
-
 		}
-		//but am i creating a new profile or FAVORITE ?
-		//create new favorite Id and insert it into the database
-		//$favorite = new Favorite(null, $requestObject->favoriterId, $_SESSION["favorite"]->getFavoriterId());
-		//$favorite->insert($pdo);
-
-//-------------------------------DELETE--------------------------------
-		else if($method === "DELETE") {
-			verifyXsrf();
-			// Retrieve the Favorite to be deleted
-			$favorite = Edu\Cnm\Flek\Favorite::getFavoriteByFavoriteeId($pdo, $profileId, $id);
-			if($favorite === null) {
-				throw(new RuntimeException("the favorite given does not exist", 404));
-			}
-			// delete favorite
-			$favorite->delete($pdo);
-			// update reply
-			$reply->message = "Unfavorite OK";
-		} else {
-			throw(new InvalidArgumentException("Invalid HTTP method request"));
+		//else{
+		//$favorites = Edu\Cnm\Flek\Favorite::getFavoriteByFavoriteeIdAndFavoriterId($pdo, $favoriterId, $favoriteeId);
+		//if($favoriterId !== null) {
+		//$reply->data = $favorite;
+		//}
 
 
+		//-------------------------POST------------------------------
+	} elseif($method === "POST") {
+		// Set XSRF cookie
+		verifyXsrf();
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+		//make sure favorite profile is available
+		//request object...whatever is on the angular form....
+
+		// make sure profileId are available
+		if(empty($requestObject->favoriteeId) === true) {
+			throw(new InvalidArgumentException("no favorite available", 405));
 		}
-		// Delete favorite
-		//$favorite->delete($pdo);
+		//if(empty($requestObject->id) === true) {
+		//throw(new InvalidArgumentException("no profile available", 405));
+		//}
+		//created new profile and insert into database
+		//$profile = new Profile(null, $requestObject->profileEmail, $requestObject->profileLocation, $requestObject->profileBio, $hash, $salt, $profileAccessToken, $profileActivationToken);
+		//$profile->insert($pdo);
 
-		//$deletedObject = new stdClass();
-		// Update reply
-		//$reply->message = "Favorite deleted OK";
+		// create new favorite and insert into the database
+		$favorite = new Edu\Cnm\Flek\Favorite(null, $requestObject->getProfileId, $requestObject->getId);
+		$favorite->insert($pdo);
+		// update reply
+		$reply->message = "Favorite created OK";
 
 	}
+	//but am i creating a new profile or FAVORITE ?
+	//create new favorite Id and insert it into the database
+	//$favorite = new Favorite(null, $requestObject->favoriterId, $_SESSION["favorite"]->getFavoriterId());
+	//$favorite->insert($pdo);
+
+//-------------------------------DELETE--------------------------------
+	else if($method === "DELETE") {
+		verifyXsrf();
+		// Retrieve the Favorite to be deleted
+		$favorite = Edu\Cnm\Flek\Favorite::getFavoriteByFavoriteeId($pdo, $profileId, $id);
+		if($favorite === null) {
+			throw(new RuntimeException("the favorite given does not exist", 404));
+		}
+		// delete favorite
+		$favorite->delete($pdo);
+		// update reply
+		$reply->message = "Unfavorite OK";
+	} else {
+		throw(new InvalidArgumentException("Invalid HTTP method request"));
+
+
+	}
+	// Delete favorite
+	//$favorite->delete($pdo);
+
+	//$deletedObject = new stdClass();
+	// Update reply
+	//$reply->message = "Favorite deleted OK";
+
+
 	// Update reply with exception information
-} catch(Exception $exception) {
+} catch
+(Exception $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 	$reply->trace = $exception->getTraceAsString();
