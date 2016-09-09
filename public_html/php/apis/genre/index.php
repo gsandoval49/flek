345bfa89c79dc9a3377a1a78e08873699e59da17
Christina Sosa <csosa2106@gmail.com>
2016-09-08 19:05:38 -0600

    commented out more code we think we don't need https://app.asana.com/0/161555263951762/170520114054336

--- public_html/php/apis/genre/index.php
+++ public_html/php/apis/genre/index.php
@@ -5,7 +5,7 @@
 /*require once here - double check if dirname(_DIR_) is needed*/
 require_once dirname(__DIR__, 2) . "/classes/autoload.php";
 require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 /*Genre will have to incorporate Image*/
 //Do we need image inserted
@@ -36,7 +36,7 @@ try {
 
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
-/*	$reply->method = $method;*/
+	/*	$reply->method = $method;*/
 
 	//sanitize the input
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
@@ -44,19 +44,19 @@ try {
 	//comment this out, use if needed, if not, delete.
 	$genreName = filter_input(INPUT_GET, "genreName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
-/*
-	// create new genre and insert into database
-	// we probably don't even need this
-	$genre = new Edu\Cnm\Flek\Genre(null, $requestObject -> genreName);
-	$genre->insert($pdo);*/
+	/*
+		// create new genre and insert into database
+		// we probably don't even need this
+		$genre = new Edu\Cnm\Flek\Genre(null, $requestObject -> genreName);
+		$genre->insert($pdo);*/
 
 
 	// make sure the id is valid for methods that require it
-/*	if(($method === "GET") && (empty($id) === true || $id < 0)) {
-		throw (new InvalidArgumentException("id cannot be empty or negative", 405));
-	} elseif(($method === "POST" || $method === "DELETE" || $method === "PUT")) {
-		throw(new \InvalidArgumentException("This action is forbidden", 405));
-	}*/
+	/*	if(($method === "GET") && (empty($id) === true || $id < 0)) {
+			throw (new InvalidArgumentException("id cannot be empty or negative", 405));
+		} elseif(($method === "POST" || $method === "DELETE" || $method === "PUT")) {
+			throw(new \InvalidArgumentException("This action is forbidden", 405));
+		}*/
 
 // handle GET request - if id is present, that genre is returned, otherwise all genres are returned
 	if($method === "GET") {
@@ -75,28 +75,28 @@ try {
 				$reply->data = $genre;
 			}
 
+			//for all other cases get all genres
+		} else {
+			$genres = Genre::getsAllGenres($pdo)->toArray();
+			if($genres !== null) {
+				$reply->data = $genres;
+			}
 		}
-	} else {
-		$genres = Genre::getsAllGenres($pdo);
-		if($genres === null) {
-			$reply->data = $genres;
-		}
-	}  {
-		throw (new InvalidArgumentException("Invalid HTTP method request"));
 	}
+	/*{
+		throw (new InvalidArgumentException("Invalid HTTP method request"));
+	}*/
+
+// update reply with exception information
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
 
-		// update reply with exception information
-	 catch(Exception $exception) {
-		$reply->status = $exception->getCode();
-		$reply->message = $exception->getMessage();
-	} catch(TypeError $typeError) {
-		$reply->status = $typeError->getCode();
-		$reply->message = $typeError->getMessage();
-	}
-
-
-
+//set up the response header
 header("Content-type: application/json");
 if($reply->data === null) {
 	unset($reply->data);
