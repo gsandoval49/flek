9a2ad9f2a2f16f526174abe1d661809e4379db07
Christina Sosa <csosa2106@gmail.com>
2016-09-09 23:58:47 -0600

    created sign up service

---
+++ public_html/angular/services/signup-service.js
@@ -0,0 +1,10 @@
+app. constant("SIGNUP_ENDPOINT", "php/apis/signup");
+app.constant("SignupService", function($http, SIGNUP_ENDPOINT) {
+	function getUrl() {
+		return(SIGNUP_ENDPOINT);
+	}
+	this.create = function(signup) {
+		console.log("inside signup service");
+		return($http.post(getUrl(), signup));
+	}
+})
