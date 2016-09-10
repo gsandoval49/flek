d631c92f3b9c4f2df78b21c3d700f5546b520646
Christina Sosa <csosa2106@gmail.com>
2016-09-10 11:20:57 -0600

    deleted genre controller

---
+++ public_html/angular/services/profile-service.js
@@ -0,0 +1,24 @@
+/**
+ * Service for profile API
+ **/
+
+app.constant("PROFILE_ENDPOINT", "php/apis/profile/");
+app.service("ProfileService", function($http, PROFILE_ENDPOINT) {
+	function getUrl() {
+		return(PROFILE_ENDPOINT);
+	}
+
+	function getUrlId(profileId) {
+		return(getUrl() + profileId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+	this.fetch = function(profileId) {
+		return(http.get(getUrl()));
+	};
+	this.updateProfile = function(profileId, profile) {
+		return($http.put(getUrlForId(profileId, profile)));
+	};
+});
