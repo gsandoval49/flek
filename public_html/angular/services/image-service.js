da807d63ec6e4d7cefd8e5e9350cc18c768a82a1
Christina Sosa <csosa2106@gmail.com>
2016-09-10 20:59:42 -0600

    created image service

--- public_html/angular/services/image-service.js
+++ public_html/angular/services/image-service.js
@@ -1,1 +1,26 @@
-app.constant("IMAGE_ENDPOINT", "php/apis/image")
+app.constant("IMAGE_ENDPOINT", "php/apis/image");
+app.service("ImageService", function($http, IMAGE_ENDPOINT) {
+	function getUrl(imageId) {
+		return(IMAGE_ENDPOINT);
+	}
+
+	function getUrlForId(imageId) {
+		return(getUrl() + imageId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function() {
+		return($http.get(getUrlForId(imageId)));
+	};
+
+	this.create = function() {
+		return($http($http.post(getUrl(), image)));
+	};
+
+	this.destroy = function() {
+		return($http.delete(getUrlForId(imageId)));
+	};
+});
