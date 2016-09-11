20ca45d479f0e7efe0c4e31b50cdc023fe7a887a
Christina Sosa <csosa2106@gmail.com>
2016-09-11 15:10:00 -0600

    added fetch all genres https://app.asana.com/0/161555263951762/170520114054348

--- public_html/angular/services/genre-service.js
+++ public_html/angular/services/genre-service.js
@@ -1,18 +1,31 @@
-app.constant("GENRE_ENDPOINT", "php/apis/genre");
+app.constant("GENRE_ENDPOINT", "php/apis/genres/");
 app.service("GenreService", function($http, GENRE_ENDPOINT) {
-	function getUrl(genreId) {
+
+	function getUrl() {
 		return(GENRE_ENDPOINT);
 	}
 
-	function getUrlForId(genreId) {
+	function getUrlForGenreId(genreId) {
 		return(getUrl() + genreId);
 	}
 
-	this.all = function() {
+	this.fetchGenreByGenreId = function(genreId) {
+		return($http.get(getUrlForGenreId(genreId)));
+	};
+
+	this.fetchAllGenres = function() {
 		return($http.get(getUrl()));
 	};
 
-	this.fetch = function() {
-		return($http.get(getUrlForId(genreId)));
+	this.create = function(genre) {
+		return($http.post(getUrl(), genre));
+	};
+
+	this.update = function(genreId, genre) {
+		return($http.put(getUrlForGenreId(genreId, genre)));
+	};
+
+	this.destroy = function(genreId) {
+		return($http.delete(getUrlForGenreId(genreId)));
 	};
 });
