d2ce76928e1bd1e8bb5a65d6961a05259cbf5c13
Robert Harding <robisonfire@gmail.com>
2016-09-11 11:06:34 -0600

    added profile from plunker for profile

--- public_html/angular/pages/profile.php
+++ public_html/angular/pages/profile.php
@@ -1,3 +1,70 @@
-<div>
-	<p>profile stuff</p>
+<div class="container" style="margin-top: 20px; margin-bottom: 20px;">
+	<div class="row panel">
+		<div class="col-md-4 bg_blur ">
+		</div>
+		<div class="col-md-8  col-xs-12">
+			<div class="header">
+				<h1>Jane Smith</h1>
+				<h4>Artist</h4>
+				<h4>Location:</h4>
+				<span>Bio: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</span>
+			</div>
+		</div>
+	</div>
+	<div class="row nav">
+		<div class="col-md-4"></div>
+		<div class="col-md-8 col-xs-12" style="margin: 0px;padding: 0px;">
+			<div class="col-md-6 col-xs-6 well">
+				<i class="fa fa-heart-o fa-lg"></i>
+			</div>
+			<div class="col-md-6 col-xs-6 well">
+				<i class="fa fa-envelope-o fa-lg"></i>
+			</div>
+		</div>
+	</div>
 </div>
+<!-- begin gallery -->
+<div class="container">
+
+	<div class="row">
+
+		<div class="col-lg-12">
+			<h1 class="page-header">Gallery</h1>
+		</div>
+		<div class="row text-center">
+			<h3>Flek Feed</h3>
+			<p>click on the images to check out the profile of the artist!</p>
+		</div>
+		<div class="row">
+			<div class="col-xs-6">
+
+				<div class="jumbotron text-center">
+					<!-- <img src="http://placehold.it/350x250"> -->
+					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
+					<!--do we want a string under the image or a modul popup-->
+					<p>image description(?)</p>
+				</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="jumbotron text-center">
+
+					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-6">
+				<div class="jumbotron text-center">
+
+					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
+				</div>
+			</div>
+			<div class="col-xs-6">
+				<div class="jumbotron text-center">
+
+					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
+				</div>
+			</div>
+		</div>
+	</div>
