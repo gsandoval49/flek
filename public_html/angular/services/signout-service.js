3586e25ce5b538f987ea32b1a2b3fa883a790439
Christina Sosa <csosa2106@gmail.com>
2016-09-11 14:29:08 -0600

    fixed more typos and missing semi colon https://app.asana.com/0/161555263951762/170520114054348

--- public_html/angular/services/signout-service.js
+++ public_html/angular/services/signout-service.js
@@ -1,7 +1,7 @@
 app.service("SignoutService", function($http) {
-	this.SIGNOUT_ENPOINT = "../../angular/controllers/signout-controller.php";
+	this.SIGNOUT_ENDPOINT = "../../angular/controllers/signout-controller.php";
 
 	this.signout = function() {
-		return($http.get(this.SIGNOUT_ENPOINT));
-	}
+		return($http.get(this.SIGNOUT_ENDPOINT));
+	};
 });
