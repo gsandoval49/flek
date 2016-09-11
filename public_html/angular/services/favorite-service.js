9c197c33569002c960a4c6d96795cdd53915d8cd
Giles Sandoval <gsandoval2277@gmail.com>
2016-09-11 16:55:29 -0600

    added favoriter and favoritee fetch, fetch all, create, destroy https://app.asana.com/0/161555263951762/170520114054348

--- public_html/angular/services/favorite-service.js
+++ public_html/angular/services/favorite-service.js
@@ -1,26 +1,45 @@
-app.constant("FAVORITE_ENDPOINT", "php/apis/favorite");
+app.constant("FAVORITE_ENDPOINT", "php/apis/favorite/");
 app.service("FavoriteService", function($http, FAVORITE_ENDPOINT) {
-	function getUrl(favoriteId) {
+
+	function getUrl() {
 		return(FAVORITE_ENDPOINT);
 	}
 
-	function getUrlForId(favoriteId) {
-		return(getUrl() + favoriteId);
+	function getUrlForId(favoriteeId) {
+		return(getUrl() + favoriteeId);
 	}
 
-	this.all = function() {
+	function getUrlForId(favoriterId) {
+		return(getUrl() + favoriterId);
+	}
+
+	/*I don't think we need this here. We're not calling an array of stuff*/
+	/*this.all = function() {
+		return($http.get(getUrl()));
+	};*/
+
+	this.fetchFavoriteByFavoriteeId = function(favoriteeId) {
+		return($http.get(getUrl() + "?favoriteeId=" + favoriteeId));
+	};
+
+	this.fetchFavoriteByFavoriterId() = function(favoriterId) {
+		return($http.get(getUrl() + "?favoriterId=" + favoriterId));
+	};
+
+	this.fetchAllFavorites = function() {
 		return($http.get(getUrl()));
 	};
 
-	this.fetch = function() {
-		return($http.get(getUrlForId(favoriteeId, favoriterId)));
+	this.create = function(favorite) {
+		return($http.post(getUrl(), favorite));
 	};
 
-	this.create = function() {
-		return($http($http.post(getUrl(), favorite)));
+	/*TODO do we need destroy if we only unpost a favorite and not destroy? for both favoriter and favoritee*/
+	this.destroy = function(favoriteeId) {
+		return($http.delete(getUrlForFavoriteeId(favoriteeId)));
 	};
 
-	this.destroy = function() {
-		return($http.delete(getUrlForId(favoriteeId, favoriterId)));
+	this.destroy = function(favoriterId) {
+		return($http.delete(getUrlForFavoriterId(favoriterId)));
 	};
 });
