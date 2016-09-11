5f4a83af1363ed8795f9be03ef099479f73f12ef
Robert Harding <robisonfire@gmail.com>
2016-09-11 11:12:46 -0600

    made sign in page

--- public_html/angular/pages/signin.php
+++ public_html/angular/pages/signin.php
@@ -1,3 +1,25 @@
-<div>
-	<p>signin stuff</p>
-</div>
+<form>
+	<div class="form-group">
+		<label for="email">Email <span class="text-danger">*</span></label>
+		<div class="input-group">
+			<div class="input-group-addon">
+				<i class="fa fa-envelope" aria-hidden="true"></i>
+			</div>
+			<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+		</div>
+	</div>
+
+	<div class="form-group">
+		<label for="password">Password <span class="text-danger">*</span></label>
+		<div class="input-group">
+			<div class="input-group-addon">
+				<i class="fa fa-comment" aria-hidden="true"></i>
+			</div>
+			<textarea class="form-control" id="password" name="password"
+						 placeholder="password"></textarea>
+		</div>
+	</div>
+
+	<button class="btn btn-success" type="submit"><i class="fa fa-pencil"></i> Sign In</button>
+	<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+</form>
