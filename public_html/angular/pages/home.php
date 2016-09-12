c801ad753c64ce4343389cd8c4f0a4febe99f9b8
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-11 23:15:19 -0600

    fixed picture size https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/home.php
+++ public_html/angular/pages/home.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-xs-12">
-        <div id="home-jumbo" class="jumbotron text-right">
+        <div id="home-jumbo" class="jumbotron home-content text-right">
             <h1><i class="pick-icon"></i> Flek</h1>
             <p class="lead">Connect artists to community. Bring skills & crafts to your home & business. Beautify your space.</p>
             <a href="signup.php" class="btn btn-warning btn-lg">Create Account</a>
