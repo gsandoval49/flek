8211f83598db1ab093407e797225ab88b8d5d90b
Christina Sosa <csosa2106@gmail.com>
2016-09-12 14:03:25 -0600

    created delete methods for profile https://app.asana.com/0/161555263951762/170520114054348

--- public_html/angular/controllers/profile-controller.js
+++ public_html/angular/controllers/profile-controller.js
@@ -338,7 +338,22 @@ app.controller('ProfileController', ["$routeParams", "$scope", "ProfileService",
 					}
 				});
 
-
-		}
-	}
-}]
+			/**
+			 * Delete Methods
+			 */
+			$scope.delete = function(profile, valited) {
+				if(validated === true) {
+					ProfileService.delete(profile)
+						.then(function(result) {
+							if(result.data.status === 200) {
+								$scope.alerts[0] = {type:"success", msg: result.data.message};
+							} else {
+								$scope.alerts[0] = {type: "danger", msg: result.data.message};
+							}
+						});
+				}
+			};
+			if($scope.profileData === null){
+				$scope.loadProfile();
+			}
+}]);
