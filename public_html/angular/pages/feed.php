7ef817c31a25297b434f76031fcd4866f7b5be39
Robert Harding <robisonfire@gmail.com>
2016-09-12 13:42:11 -0600

    changed code for feed page https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/feed.php
+++ public_html/angular/pages/feed.php
@@ -1,37 +1,55 @@
+
 <div class="container">
-<div class="row text-center">
-	<h3>Flek Feed</h3>
-	<p>click on the images to check out the profile of the artist!</p>
-</div>
-<div class="row">
-	<div class="col-xs-6">
-
-		<div class="jumbotron text-center">
-
-			<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src=""/></a>
-			<!--do we want a string under the image or a modul popup-->
-			<p>image description(?)</p>
+	<!-- Page Header -->
+	<div class="row">
+		<div class="col-lg-12">
+			<h1 class="page-header">Flek Feed
+			</h1>
 		</div>
 	</div>
-	<div class="col-xs-6">
-		<div class="jumbotron text-center">
+	<!-- /.row -->
+	<!-- Projects Row -->
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
 
-			<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src=""</a>
+
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
 		</div>
 	</div>
-</div>
-<div class="row">
-	<div class="col-xs-6">
-		<div class="jumbotron text-center">
-
-			<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src=""/></a>
+	<!-- /.row -->
+	<!-- Projects Row -->
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
 		</div>
 	</div>
-	<div class="col-xs-6">
-		<div class="jumbotron text-center">
 
-			<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src=""/></a>
-		</div>
-	</div>
 </div>
-</div>
+
+
