6e1383262ec643f8a7d3115872ff1fb29b168b5b
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-12 15:54:56 -0600

    fixed containers for profile https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/profile.php
+++ public_html/angular/pages/profile.php
@@ -1,68 +1,89 @@
-<div class="container">
+<div class="profile-container">
 	<div class="row">
-		<div class="col-md-4">
+		<div class="col-sm-6 col-md-3">
 		</div>
-		<div class="col-md-8  col-xs-12">
-			<div>
+		</div>
+	</div>
+	<div class="profile-content">
+		<div class="col-md-6 col-md-9">
+			<div class="row">
+			<div class="header">
 				<h1>Jane Smith</h1>
 				<h4>Artist</h4>
 				<h4>Location:</h4>
 				<span>Bio: Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
 "There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</span>
+				</div>
 			</div>
-		</div>
 	</div>
-	<div class="row">
+</div>
+
+	<div class="row nav">
 		<div class="col-md-4"></div>
 		<div class="col-md-8 col-xs-12">
 			<div class="col-md-6 col-xs-6 ">
 				<button href="" class="btn btn-default" type="submit"><i class="fa fa-heart-o fa-lg"></i>Favorite</button>
 			</div>
-			<div class="col-md-6 col-xs-6 ">
-				<button class="btn btn-default" type="submit"><i class="fa fa-envelope-o fa-lg"></i>Message</button>
+				<div class="col-md-6 col-xs-6 ">
+					<button class="btn btn-default" type="submit"><i class="fa fa-envelope-o fa-lg"></i>message</button>
 
 
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
-<!-- begin gallery -->
+
+
+
+	<!-- begin gallery -->
 <div class="container">
-
+	<!-- Page Header -->
 	<div class="row">
-
-		<div class="row text-center">
-			<h3>Flek Feed</h3>
-			<p>click on the images to view a description of the piece!</p>
+		<div class="col-lg-12">
+			<h1 class="page-header">Profile Feed
+			</h1>
 		</div>
-		<div class="row">
-			<div class="col-xs-6">
+	</div>
+	<!-- /.row -->
+	<!-- Projects Row -->
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
 
-				<div class="jumbotron text-center">
-					<!-- <img src="http://placehold.it/350x250"> -->
-					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
-				</div>
-			</div>
-			<div class="col-xs-6">
-				<div class="jumbotron text-center">
 
-					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
-				</div>
-			</div>
+			</h3>
 		</div>
-		<div class="row">
-			<div class="col-xs-6">
-				<div class="jumbotron text-center">
-
-					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
-				</div>
-			</div>
-			<div class="col-xs-6">
-				<div class="jumbotron text-center">
-
-					<a href="profile.php" class="btn btn-warning btn-lg"><img class="feedPic" src="http://placehold.it/350x250"/></a>
-				</div>
-			</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<!-- /.row -->
+	<!-- Projects Row -->
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
 		</div>
 	</div>
 
+</div>
