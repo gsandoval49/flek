7050f2381f6e7287266dbc5eded029eb048b44a4
Christina Sosa <csosa2106@gmail.com>
2016-09-12 21:58:01 -0600

    included ng to signin form https://app.asana.com/0/161555263951762/170520114054336

--- public_html/angular/pages/signin.php
+++ public_html/angular/pages/signin.php
@@ -2,27 +2,30 @@
 <div class="col-sm-4">
 	<div class="form-group">
 		<label for="email">Email <span class="text-danger">*</span></label>
-		<div class="input-group" ng-class="{ 'has-error': profileSignupForm.full}">
+		<div class="input-group" ng-class="{ 'has-error': signinForm.profileEmail.$touched && signinForm.profileEmail.$invalid}">
+
 			<div class="input-group-addon">
 				<i class="fa fa-envelope" aria-hidden="true"></i>
 			</div>
-			<input type="email" class="form-control" id="email" name="email" placeholder="Email" ng-minlength="4" ng-maxlength="128" ng-required="true"/>
+
+			<input type="email" class="form-control" id="profileEmail" name="profileEmail" placeholder="Email" ng-required="true"/>
 		</div>
-		<div class="alert alert-danger" role="alert" ng-messages="profileSignupForm.$error" ng-if="profileSignupForm.$touched" ng-hide="profileSignupForm.$valid">
-			<p ng-message="minlength">Email is too short.</p>
-			<p ng-message="maxlength">Email is too long.</p>
-			<p ng-message="required">Please enter your email.</p>
+
+		<div class="alert alert-danger" role="alert" ng-messages="signinForm.profileEmail.$error" ng-if="signinForm.$touched" ng-hide="signinForm.profileEmail.$valid">
+			<p ng-message="required">Email Required</p>
 		</div>
 	</div>
 
 	<div class="form-group">
 		<label for="password">Password <span class="text-danger">*</span></label>
-		<div class="input-group">
+		<div class="input-group" ng-class="{ 'has-error': signinForm.profilePassword.$touched && signinForm.profilePassword.$invalid}">
+
 			<div class="input-group-addon">
 				<i class="fa fa-comment" aria-hidden="true"></i>
 			</div>
-			<input class="form-control" type="password" name="password"
-						 placeholder="password">
+
+			<input class="form-control" type="password" id="profilePassword" name="password" placeholder="password" ng-required="true"/>
+			<p ng-message="required">Password Required</p>
 		</div>
 	</div>
 
