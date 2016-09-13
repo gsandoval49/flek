8d8a90e26feaa207ce620bb6a8b1ae8e5afe711c
Robert Harding <robisonfire@gmail.com>
2016-09-13 16:24:13 -0600

    got button on home page to redirect to sign up page https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/home.php
+++ public_html/angular/pages/home.php
@@ -3,7 +3,8 @@
         <div id="home-jumbo" class="jumbotron home-content text-right">
             <h1><i class="pick-icon"></i> Flek</h1>
             <p class="lead">Connect artists to community. Bring skills & crafts to your home & business. Beautify your space.</p>
-            <a href="https://bootcamp-coders.cnm.edu/~rharding6/flek/public_html/signup" class="btn btn-default btn-lg">Create Account</a>
+            <a href="https://bootcamp-coders.cnm.edu/~rharding6/flek/public_html/signup" class="btn btn-default btn-lg"><i class="btn btn-default btn-lg">Create Account</i></a>
+
         </div>
     </div>
 </div>
