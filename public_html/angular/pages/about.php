e153429824c666dec7e9d963684fa8d28121395c
Robert Harding <robisonfire@gmail.com>
2016-09-13 15:46:07 -0600

    updated about page https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/about.php
+++ public_html/angular/pages/about.php
@@ -1,19 +1,26 @@
 <div class="row">
 	<div class="col-xs-12">
-		<div id="about-jumbo" class="jumbotron about-content">
+		<div id="about-jumbo" class="jumbotron about-content text-center">
 			<h1><i class="icon-about"></i> Beautify your space</h1>
 			<p class="leader">Be on the front line of something cool</p>
+			<h4>Flek is a free-to-use website built for the purpose of facilitating the continual growth of a vibrant artistic community. Anyone is welcome to join the network; artists and art lovers alike. After profile creation, it is time to start connecting! Upload pictures of your best pieces or space you have available to display others' work. Message other users or favorite other profiles for easy access.</h4>
+			<div>
+				<div>
+					<h3>Flek will....</h3>
+				</div>
+				<div>
+					<!-- this section may need some TLC-->
+					<p>Connect artists with the art loving community.</p>
+					<p>Bring skills & crafts to your home & business; beautify your space.</p>
+					<p>Discover something unexpected.</p>
+					<p>Foster meaningful relationships.</p>
+					<p>Create new connections.</p>
+				</div>
+			</div>
 		</div>
+
 	</div>
 </div>
 
-<div class="container-fluid">
-	<div class="row">
-		<h3>Flek is....</h3>
-	</div>
-	<div class="row">
-		<!-- this section may need some TLC-->
-		<p>Connect artists with the art loving community. Bring skills & crafts to your home & business; beautify your space.</p>
-	</div>
-</div>
+
 
