01c6275c7d02276d5d3687834e26b3fec08f76c8
Christina Sosa <csosa2106@gmail.com>
2016-09-13 09:47:24 -0600

    moved ng-message into messages div tag https://app.asana.com/0/161555263951762/170520114054336

--- public_html/angular/pages/signin.php
+++ public_html/angular/pages/signin.php
@@ -3,41 +3,51 @@
 
 		<!--form starts here-->
 		<form name="signinForm" id="signinForm" ng-submit="signin(signinData, signinForm.$valid);" novalidate>
-<div class="jumbotron col-sm-4 col-lg-offset-4">
-	<div class="row">
-	<div class="form-group">
-		<label for="email">Email <span class="text-danger">*</span></label>
-		<div class="input-group" ng-class="{ 'has-error': signinForm.profileEmail.$touched && signinForm.profileEmail.$invalid}">
+			<div class="jumbotron col-sm-4 col-lg-offset-4">
+				<div class="row">
+					<div class="form-group">
+						<label for="email">Email <span class="text-danger">*</span></label>
+						<div class="input-group"
+							  ng-class="{ 'has-error': signinForm.profileEmail.$touched && signinForm.profileEmail.$invalid}">
 
-			<div class="input-group-addon">
-				<i class="fa fa-envelope" aria-hidden="true"></i>
+							<div class="input-group-addon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+
+							<input type="email" class="form-control" id="profileEmail" name="profileEmail" placeholder="Email"
+									 ng-required="true" ng-model="signinData.profileEmail"/>
+						</div>
+
+						<div class="alert alert-danger" role="alert" ng-messages="signinForm.profileEmail.$error"
+							  ng-if="signinForm.$touched" ng-hide="signinForm.profileEmail.$valid">
+							<p ng-message="required">Email Required</p>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="password">Password <span class="text-danger">*</span></label>
+						<div class="input-group"
+							  ng-class="{ 'has-error': signinForm.profilePassword.$touched && signinForm.profilePassword.$invalid}">
+
+							<div class="input-group-addon">
+								<i class="fa fa-comment" aria-hidden="true"></i>
+							</div>
+
+							<input class="form-control" type="password" id="profilePassword" name="profilePassword"
+									 placeholder="password" ng-required="true" ng-model="signinData.profilePassword"/>
+
+							<div class="alert alert-danger" role="alert" ng-messages="signinForm.profilePassword.$error"
+								  ng-if="signinForm.$touched" ng-hide="signinForm.profilePassword.$valid">
+								<p ng-message="required">Password Required</p>
+							</div>
+						</div>
+					</div>
+
+					<button class="btn btn-success" type="submit"><i class="fa fa-pencil"></i> Sign In</button>
+				</div>
 			</div>
+		</form>
 
-			<input type="email" class="form-control" id="profileEmail" name="profileEmail" placeholder="Email" ng-required="true" ng-model="signinData.profileEmail"/>
-		</div>
-
-		<div class="alert alert-danger" role="alert" ng-messages="signinForm.profileEmail.$error" ng-if="signinForm.$touched" ng-hide="signinForm.profileEmail.$valid">
-			<p ng-message="required">Email Required</p>
-		</div>
-	</div>
-
-	<div class="form-group">
-		<label for="password">Password <span class="text-danger">*</span></label>
-		<div class="input-group" ng-class="{ 'has-error': signinForm.profilePassword.$touched && signinForm.profilePassword.$invalid}">
-
-			<div class="input-group-addon">
-				<i class="fa fa-comment" aria-hidden="true"></i>
-			</div>
-
-			<input class="form-control" type="password" id="profilePassword" name="profilePassword" placeholder="password" ng-required="true" ng-model="signinData.profilePassword"/>
-			<p ng-message="required">Password Required</p>
-		</div>
-	</div>
-
-	<button class="btn btn-success" type="submit"><i class="fa fa-pencil"></i> Sign In</button>
-	</div>
-	</div>
-</form>
 		<!--Row for Sign In Message-->
 		<section id="welcome" class="message bg primary text-center">
 			<div class="container">
@@ -49,7 +59,8 @@
 				</div>
 			</div>
 		</section>
-		</div>
+
+	</div>
 </div>
 
 
