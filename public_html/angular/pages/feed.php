bc80f22a416d428473a2486e3ac14f8b03c908ff
Robert Harding <robisonfire@gmail.com>
2016-09-13 11:13:42 -0600

    made space for lots of pics in feed https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/feed.php
+++ public_html/angular/pages/feed.php
@@ -1,5 +1,5 @@
 
-<div class="container">
+<div>
 	<!-- Page Header -->
 	<div class="row">
 		<div class="col-lg-12">
@@ -31,6 +31,186 @@
 	</div>
 	<!-- /.row -->
 	<!-- Projects Row -->
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+		<div class="col-md-6 portfolio-item">
+			<a href="#">
+				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+			</a>
+			<h3>
+				<a href="#">Profile</a>
+			</h3>
+		</div>
+	</div>
 	<div class="row">
 		<div class="col-md-6 portfolio-item">
 			<a href="#">
