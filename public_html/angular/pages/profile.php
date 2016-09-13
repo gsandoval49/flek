22cc793684db37a89c88cf12578078b014d5f9e8
Giles Sandoval <gsandoval2277@gmail.com>
2016-09-13 13:13:52 -0600

    adapted css and profile bootstrap to get picture and index to work. https://app.asana.com/0/161555263951762/170520114054348

--- public_html/angular/pages/profile.php
+++ public_html/angular/pages/profile.php
@@ -1,42 +1,37 @@
-<div class="profile-container">
+<!--container for profile picture & profile content-->
+<div class="container">
 	<div class="row">
-		<div class="col-md-6 col-lg-offset-8">
+		<div class="col-md-4 profile-picture"></div>
+		<div class="col-md-4">
+			<h1>Jane Smith</h1>
+			<h4>Artist</h4>
+			<h4>Location:</h4>
+			<p>Bio: Artist, blogger, writer, consultant. Paints poetic landscapes, nature, birds, and still like paintings.
+				http://janesmith.com</p>
+			<p>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is
+				pain..."</p>
 		</div>
-	</div>
-</div>
-
-<!--Profile Content-->
-<div class="profile-content">
-		<div class="col-md-6 text-center">
-			<div class="row">
-			<div class="header">
-				<h1>Jane Smith</h1>
-				<h4>Artist</h4>
-				<h4>Location:</h4>
-				<p>Bio: Artist, blogger, writer, consultant. Paints poetic landscapes, nature, birds, and still like paintings. http://janesmith.com</p>
-				<p>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</p>
-				</div>
-			</div>
-	</div>
-</div>
-
-	<div class="row nav">
-		<div class="col-md-4"></div>
-		<div class="col-md-8 col-xs-12">
-			<div class="col-md-6 col-xs-6 ">
+		<div>
+			<div class="col-md-4">
 				<button href="" class="btn btn-default" type="submit"><i class="fa fa-heart-o fa-lg"></i>Favorite</button>
 			</div>
-				<div class="col-md-6 col-xs-6 ">
-					<button class="btn btn-default" type="submit"><i class="fa fa-envelope-o fa-lg"></i>message</button>
-
-
-				</div>
+			<div class="col-md-4">
+				<button class="btn btn-default" type="submit"><i class="fa fa-envelope-o fa-lg"></i>message</button>
 			</div>
 		</div>
+	</div>
+</div>
 
 
+<!--<div class="row nav">
+	<div class="col-md-4"></div>
+	<div class="col-md-4 col-xs-12">
 
-	<!-- begin gallery -->
+	</div>
+</div>-->
+
+
+<!-- begin gallery -->
 <div class="container">
 	<!-- Page Header -->
 	<div class="row">
@@ -50,7 +45,7 @@
 	<div class="row">
 		<div class="col-md-6 portfolio-item">
 			<a href="#">
-				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+				<img class="img-responsive" src="http://placehold.it/700x400" alt=""/>
 			</a>
 			<h3>
 				<a href="#">Profile</a>
@@ -60,7 +55,7 @@
 		</div>
 		<div class="col-md-6 portfolio-item">
 			<a href="#">
-				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+				<img class="img-responsive" src="http://placehold.it/700x400" alt=""/>
 			</a>
 			<h3>
 				<a href="#">Profile</a>
@@ -72,7 +67,7 @@
 	<div class="row">
 		<div class="col-md-6 portfolio-item">
 			<a href="#">
-				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+				<img class="img-responsive" src="http://placehold.it/700x400" alt=""/>
 			</a>
 			<h3>
 				<a href="#">Profile</a>
@@ -80,7 +75,7 @@
 		</div>
 		<div class="col-md-6 portfolio-item">
 			<a href="#">
-				<img class="img-responsive" src="http://placehold.it/700x400" alt="" />
+				<img class="img-responsive" src="http://placehold.it/700x400" alt=""/>
 			</a>
 			<h3>
 				<a href="#">Profile</a>
