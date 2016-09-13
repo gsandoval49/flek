9b0612154af0805bbc0e1cdeaf2c218b38bf3be0
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-13 13:21:28 -0600

    set img width and height for images added two more https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/feed.php
+++ public_html/angular/pages/feed.php
@@ -12,7 +12,7 @@
 		<div class="row">
 			<div class="col-md-6 portfolio-item">
 				<a href="#">
-					<img id="drawing" class="img-responsive" src="images/feed-drawing-1" alt=""/>
+					<img id="drawing" class="img-responsive" src="images/feed-drawing-1" alt="" height="400", width=700"/>
 				</a>
 				<h3>
 					<a href="#">Profile</a>
@@ -22,7 +22,7 @@
 			</div>
 			<div class="col-md-6 portfolio-item">
 				<a href="#">
-					<img class="img-responsive" src="images/feed-drawing-2" alt=""/>
+					<img class="img-responsive" src="images/feed-drawing-2" alt="" height="400", width=700"/>
 				</a>
 				<h3>
 					<a href="#">Profile</a>
