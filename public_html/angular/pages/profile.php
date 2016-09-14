c34a82e68a67f733f420ec3bcee7d8cc4053567f
Robert Harding <robisonfire@gmail.com>
2016-09-14 11:55:26 -0600

    rearranged profile https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/profile.php
+++ public_html/angular/pages/profile.php
@@ -1,8 +1,8 @@
 <!--container for profile picture & profile content-->
 <div class="container">
 	<div class="row">
-		<div class="col-md-4 profile-picture"></div>
-		<div class="col-md-6">
+		<div class="col-md-3 profile-picture"></div>
+		<div class="col-md-4">
 			<h1>Staci Parker</h1>
 			<h4>Artist</h4>
 			<h4>Location: Albuquerque, NM</h4>
@@ -11,52 +11,84 @@
 			<p>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is
 				pain..."</p>
 		</div>
-		<div class="col-md-2" ng-controller="MailController">
-			<!-- favorite button section -->
-			<h4>Favorite My Profile</h4>
-			<button href="" class="btn btn-default" type="submit"><i class="fa fa-heart-o fa-lg"></i>Favorite</button>
+		<!-- form group -->
+		<div class="profile-formcontainer">
+			<div class="row">
+				<div class="col-md-4">
+					<form name="uploadForm" id="uploadForm" class="form-horizontal well"
+							ng-controller=""
+							ng-submit="submit(formData, uploadForm.$valid);" novalidate>
+						<div class="container-fluid">
 
-			<!--message section -->
-<!--			<h4>Send Me A Message</h4>
-			<button class="btn btn-default" type="button" ng-click="touched = !touched" ><i class="fa fa-envelope-o fa-lg"></i>message
-			</button>-->
-			<!--<div class="col-xs-12 col-md-6  col-md-offset-2">--> <!--took this away. it's got gray i don't need.-->
+							<label class="control-label">Select File</label>
+							<input id="input-1" type="file" class="file">
+
+							<div class="form-group">
+								<label for="genre">genre</label>
+								<div class="input-group">
+									<select>
+										<option value="painting">Painting</option>
+										<option value="sculpture">Sculpture</option>
+										<option value="drawing">Drawing</option>
+										<option value="graffiti">Graffiti</option>
+										<option value="graphic-design">Graphic Design</option>
+										<option value="fashion">Fashion</option>
+									</select>
+
+									<!--<input class="form-control ng-untouched ng-pristine ng-invalid" id="genre" name="genre" ng-reflect-name="genre"
+											 type="text">-->
+								</div>
+								<div class="alert alert-danger" ng-reflect-hidden="true" hidden="">
+									genre is required
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="imageDescription">Image Description</label>
+								<div class="input-group">
+
+									<input class="form-control ng-untouched ng-pristine ng-invalid" id="imageDescription"
+											 name="imageDescription" required=""
+											 ng-reflect-model="" ng-reflect-name="imageDescription" type="imageDescription">
+								</div>
+								<div class="alert alert-danger" ng-reflect-hidden="true" hidden="">
+									Image Description is required
+								</div>
+							</div>
+							<div class="form-group">
+								<label for="tags">Tags</label>
+								<div class="input-group">
+
+									<input class="form-control ng-untouched ng-pristine ng-invalid" id="tags" name="tags"
+											 required=""
+											 ng-reflect-model="" ng-reflect-name="tags" type="text">
+								</div>
+								<div class="alert alert-danger" ng-reflect-hidden="true" hidden="">
+									Tag is required
+								</div>
+							</div>
+
+							<div class="form-group">
+								<button class="btn btn-lg btn-warning" type="upload"> Upload</button>
+								<button class="btn btn-lg btn-default" type="reset">Clear</button>
+							</div>
 
 
-				<!--Begin Contact Form-->
-<!--				<form name="messageForm" class="form-horizontal well" ng-controller="MessageController"
-						ng-submit="submit(formData, messageForm.$valid);" id="messageForm" action="../php/mailer.php"
-						method="post" novalidate>-->
-
-					<!--<h4>Contact Staci:</h4>-->
-
-						<div class="alert alert-danger" role="alert" ng-messages="messageForm.name.$error"
-							  ng-if="messageForm.name.$touched" ng-hide="messageForm.name.$valid">
-							<p ng-message="min">Your name is too small.</p>
-							<p ng-message="max">Your name is too large.</p>
-							<p ng-message="required">Please enter your name.</p>
 						</div>
-
-					</div> <!-- form group -->
-
+					</form>
+				</div>
+			</div>
+		</div>
 
 
 		<!-- form-group -->
 
-					<!--empty area for form error/success output-->
-					<div class="row">
-						<div class="col-xs-6 col-md-6 col-md-offset-5">
-							<div id="output-area"></div>
-						</div>
-					</div>
-
-				</form>
+		<!--empty area for form error/success output-->
 
 
-			</div> <!-- row -->
+	</div> <!-- row -->
 
-		</div>
-	</div>
+</div>
+</div>
 </div>
 
 
@@ -66,68 +98,14 @@
 
 	</div>
 </div>-->
-<div class="profile-formcontainer">
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<form name="contactForm" id="contactForm" class="form-horizontal well" ng-controller="ContactFormController" ng-submit="submit(formData, contactForm.$valid);" novalidate>
-				<div class="container-fluid">
-
-					<label class="control-label">Select File</label>
-					<input id="input-1" type="file" class="file">
-
-					<div class="form-group">
-						<label for="genre">genre</label>
-						<div class="input-group">
-							<select>
-								<option value="painting">Painting</option>
-								<option value="sculpture">Sculpture</option>
-								<option value="drawing">Drawing</option>
-								<option value="graffiti">Graffiti</option>
-								<option value="graphic-design">Graphic Design</option>
-								<option value="fashion">Fashion</option>
-							</select>
-
-							<!--<input class="form-control ng-untouched ng-pristine ng-invalid" id="genre" name="genre" ng-reflect-name="genre"
-									 type="text">-->
-						</div>
-						<div class="alert alert-danger" ng-reflect-hidden="true" hidden="">
-							genre is required
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="imageDescription">Image Description</label>
-						<div class="input-group">
-
-							<input class="form-control ng-untouched ng-pristine ng-invalid" id="imageDescription" name="imageDescription" required=""
-									 ng-reflect-model="" ng-reflect-name="imageDescription" type="imageDescription">
-						</div>
-						<div class="alert alert-danger" ng-reflect-hidden="true" hidden="">
-							Image Description is required
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="tags">Tags</label>
-						<div class="input-group">
-
-							<input class="form-control ng-untouched ng-pristine ng-invalid" id="tags" name="tags" required=""
-									 ng-reflect-model="" ng-reflect-name="tags" type="text">
-						</div>
-						<div class="alert alert-danger" ng-reflect-hidden="true" hidden="">
-							Tag is required
-						</div>
-					</div>
-
-					<div class="form-group">
-						<button class="btn btn-lg btn-warning" type="upload"   > Upload</button>
-						<button class="btn btn-lg btn-default" type="reset">Clear</button>
-					</div>
+<div>
+	<!-- favorite button section -->
+	<h4>Favorite My Profile</h4>
+	<button href="" class="btn btn-default" type="submit"><i class="fa fa-heart-o fa-lg"></i>Favorite</button>
 
 
-				</div>
-			</form>
-		</div>
-	</div>
 </div>
+
 
 <!-- begin gallery -->
 <div class="container">
