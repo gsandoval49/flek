68e8fb09c381e7aa11e56d4d48625532ae50de3c
Diane Peshlakai <dpeshlakai90@gmail.com>
2016-09-13 18:50:34 -0600

    took out extra design not needed https://app.asana.com/0/161555263951762/170520114054346

--- public_html/angular/pages/about.php
+++ public_html/angular/pages/about.php
@@ -9,12 +9,6 @@
 				creation, it is time to start connecting! Upload pictures of your best pieces or space you have available to
 				display others' work. Message other users or favorite other profiles for easy access.</h4>
 			<div>
-				<div class="about-container">
-				<div class="row">
-					<div class="col-md-4">
-						<i class="fa fa-paint-brush" aria-hidden="true"></i>
-					</div>
-					<div class="col-md-4">
 						<!-- this section may need some TLC-->
 						<h2>Flek will....</h2>
 						<p>Connect artists with the art loving community.</p>
@@ -23,16 +17,13 @@
 						<p>Foster meaningful relationships.</p>
 						<p>Create new connections.</p>
 					</div>
-					<div class="col-md-4">.col-md-4
-					</div>
 						</div>
 					<div>
 				</div>
 			</div>
 		</div>
 
-	</div>
-</div>
+
 
 
 
