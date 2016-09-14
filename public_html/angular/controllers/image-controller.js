e5a5c637a9809d83d9da8c9a52af2b4411b4b424
Giles Sandoval <gsandoval2277@gmail.com>
2016-09-14 12:57:18 -0600

    deleted the code that i wasted my time on :) https://app.asana.com/0/161555263951762/170520114054348

--- public_html/angular/controllers/image-controller.js
+++ public_html/angular/controllers/image-controller.js
@@ -50,27 +50,11 @@ app.controller('ImageController', ["$scope", "ImageService","$location", functio
 			})
 	};
 	/**
-	 * add method to upload image file
-	 * also - embedded within the add or upload image file
 	 * we create an image and sends it to the image API
 	 *
 	 * @param image
 	 * @param validated true if Angular validated the form, false if not
 	 **/
-	$scope.add = function(image, validated) {
-		if(validated === true) {
-			ImageService.add.(image)
-				.then(function(){
-					var image = document.getElementById("image").images[0],
-						run = new FileReader();
-					run.onloadend = function(execute){
-						var data = execute.target.result;
-						// add our API key call to cloudinary
-					};
-					run.readAsArrayBuffer(image);
-				});
-		}
-	};
 	$scope.createImage = function(image, validated) {
 		if(validated === true) {
 			ImageService.create(image)
